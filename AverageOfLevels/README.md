2534d648664e1add5e35fb5f544bfa5eb98144fb
silan-liu <413769707@qq.com>
2018-05-22 19:23:14 +0800

    add problem

---
+++ AverageOfLevels/README.md
@@ -0,0 +1,3 @@
+# AverageOfLevels
+
+A description of this package.
---
+++ AverageOfLevels/Sources/AverageOfLevels/main.swift
@@ -0,0 +1,82 @@
+// https://leetcode.com/problems/average-of-levels-in-binary-tree/description/
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+            self.val = val
+            self.left = nil
+            self.right = nil
+    }
+}
+
+func averageOfLevels(_ root: TreeNode?) -> [Double] {
+    var queue = [(TreeNode, Int)]()
+    var map = [Int: [TreeNode]]()
+    var result = [Double]()
+    
+    if let root = root {
+        result.append(Double(root.val))
+        
+        queue.append((root, 0))
+        
+        // 横向遍历，map记录所有level的节点
+        while queue.count > 0 {
+            let (node, level) = queue[0]
+            queue.removeFirst()
+            
+            if let left = node.left {
+                if var nodes = map[level + 1] {
+                    nodes.append(left)
+                    map[level + 1] = nodes
+                } else {
+                    var nodes = [TreeNode]()
+                    nodes.append(left)
+                    
+                    map[level + 1] = nodes
+                }
+                
+                queue.append((left, level + 1))
+            }
+            
+            if let right = node.right {
+                if var nodes = map[level + 1] {
+                    nodes.append(right)
+                    map[level + 1] = nodes
+                } else {
+                    var nodes = [TreeNode]()
+                    nodes.append(right)
+                    
+                    map[level + 1] = nodes
+                }
+                
+                queue.append((right, level + 1))
+            }
+        }
+        
+        // 按层排序
+        let sortedMap = map.sorted(by: {
+            $0.0 < $1.0
+        })
+        
+        // 计算每层的平均值
+        for (_, value) in sortedMap {
+            if value.count == 0 {
+                continue
+            }
+            
+            var sum: Double = 0
+            for item in value {
+                sum += Double(item.val)
+            }
+            
+            let average = sum / Double(value.count)
+            
+            result.append(average)
+        }
+        
+        return result
+    } else {
+        return [0]
+    }
+}
