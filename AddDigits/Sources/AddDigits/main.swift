830789cf94f759b49373a5df54b15b9732d31e89
silan-liu <413769707@qq.com>
2018-06-03 11:37:43 +0800

    最优解

--- AddDigits/Sources/AddDigits/main.swift
+++ AddDigits/Sources/AddDigits/main.swift
@@ -1,5 +1,6 @@
 print("Hello, world!")
 
+// 常规做法
 func addDigits(_ num: Int) -> Int {
     if num >= 0 && num <= 9 {
         return num
@@ -17,4 +18,14 @@ func addDigits(_ num: Int) -> Int {
     return addDigits(sum)
 }
 
+// 最优解
+// https://leetcode.com/problems/add-digits/discuss/68580/Accepted-C++-O(1)-time-O(1)-space-1-Line-Solution-with-Detail-Explanations
+// digital root https://en.wikipedia.org/wiki/Digital_root#Congruence_formula
+// base = 10
+// dr(n) = 0 if n == 0
+// dr(n) = (b-1) if n != 0 and n % (b-1) == 0
+// dr(n) = n mod (b-1) if n % (b-1) != 0
+// or dr(n) = 1 + (n - 1) % (b - 1)
 print(addDigits(38))
+
+
