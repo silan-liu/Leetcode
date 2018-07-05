d65f7b53dd8b9d5da8405a4f17a88041511ca94f
silan-liu <413769707@qq.com>
2018-07-05 11:43:47 +0800

    recursiveReverseList

--- reverseList/Sources/reverseList/main.swift
+++ reverseList/Sources/reverseList/main.swift
@@ -28,6 +28,33 @@ func reverseList(_ head: ListNode?) -> ListNode? {
     return next
 }
 
+func reverseList_2(_ head: ListNode?) -> ListNode? {
+    var node: ListNode? = nil
+    let _ = recursiveReverseList(head, &node)
+    
+    return node
+}
+
+func recursiveReverseList(_ head: ListNode?, _ result: inout ListNode?) -> ListNode? {
+    if let head = head {
+        let node = ListNode(head.val)
+        let pre = recursiveReverseList(head.next, &result)
+        if let pre = pre {
+            pre.next = node
+        }
+        
+        // recursiveReverseList(1), ..., recursiveReverseList(5)
+        // 记录最后的结点，因为递归，最里层会先调用,即recursiveReverseList(5)会先执行，此时result设置为5这个结点。
+        if result == nil {
+            result = node
+        }
+        
+        return node
+    }
+    
+    return nil
+}
+
 var head = ListNode(1)
 var tmp: ListNode = head
 for i in 2...5 {
@@ -40,4 +67,6 @@ for i in 2...5 {
 tmp.next = nil
 
 let _ = reverseList(head)
+reverseList_2(head)
+
  
