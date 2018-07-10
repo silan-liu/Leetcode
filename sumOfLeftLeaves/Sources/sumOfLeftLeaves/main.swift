25722aee0cb52e21ab402e3788fa441e6747cc29
silan-liu <413769707@qq.com>
2018-07-10 10:28:58 +0800

    add sumOfLeftLeaves_2

--- sumOfLeftLeaves/Sources/sumOfLeftLeaves/main.swift
+++ sumOfLeftLeaves/Sources/sumOfLeftLeaves/main.swift
@@ -18,6 +18,24 @@ func sumOfLeftLeaves(_ root: TreeNode?) -> Int {
     return sum
 }
 
+func sumOfLeftLeaves_2(_ root: TreeNode?) -> Int {
+    if root == nil {
+        return 0
+    }
+    
+    // 叶子
+    if root?.left == nil, root?.right == nil {
+        return 0
+    }
+    
+    // 左节点为叶子
+    if let left = root?.left, left.left == nil, left.right == nil {
+        return left.val + sumOfLeftLeaves(root?.right)
+    } else {
+        return sumOfLeftLeaves(root?.left) + sumOfLeftLeaves(root?.right)
+    }
+}
+
 func recursive(_ root: TreeNode?, _ isLeft: Bool, _ sum: inout Int) {
     if let root = root {
         // 如果是叶子节点，并且是左子树
