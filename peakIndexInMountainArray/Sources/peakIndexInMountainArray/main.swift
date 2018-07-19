430360ed5e672ab6db31f4a45c30ccb5cf34c5c5
silan-liu <413769707@qq.com>
2018-07-19 09:20:25 +0800

    add https://leetcode.com/problems/peak-index-in-a-mountain-array/description/

---
+++ peakIndexInMountainArray/Sources/peakIndexInMountainArray/main.swift
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/description/
+func peakIndexInMountainArray(_ A: [Int]) -> Int {
+    var max = Int.min
+    var maxIndex = 0
+    var index = 0
+    for num in A {
+        if num > max {
+            max = num
+            maxIndex = index
+        }
+        
+        index += 1
+    }
+    
+    return maxIndex
+}
+
+let numbers = [0,2,1,0]
+print(peakIndexInMountainArray(numbers))
