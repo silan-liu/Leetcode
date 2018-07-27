b0fffe88f089b5fb3aabf0ebc5f2e2a6968f6b63
silan-liu <413769707@qq.com>
2018-07-27 08:53:19 +0800

    add solution

--- missingNumber/Sources/missingNumber/main.swift
+++ missingNumber/Sources/missingNumber/main.swift
@@ -17,5 +17,21 @@ func missingNumber(_ nums: [Int]) -> Int {
     return 0
 }
 
+// 用0-n之和减去nums之和，即为结果
+func missingNumber_2(_ nums: [Int]) -> Int {
+    let count = nums.count
+    
+    var sum = 0
+    for i in 0...count {
+        sum += i
+    }
+    
+    for n in nums {
+        sum -= n
+    }
+    
+    return sum
+}
+
 let nums = [9,6,4,2,3,5,7,0,1]
-print(missingNumber(nums))
+print(missingNumber_2(nums))
