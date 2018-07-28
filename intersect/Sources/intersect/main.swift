d9bc3c7822a06705ed452f40bdf00874f7ec4080
silan-liu <413769707@qq.com>
2018-07-28 23:17:02 +0800

    add https://leetcode.com/problems/intersection-of-two-arrays-ii/description/

--- intersect/Sources/intersect/main.swift
+++ intersect/Sources/intersect/main.swift
@@ -1,1 +1,25 @@
-print("Hello, world!")
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
+    let count1 = nums1.count
+    let count2 = nums2.count
+    
+    var longArray = count1 > count2 ? nums1 : nums2
+    let shortArray = count1 <= count2 ? nums1 : nums2
+    
+    var result = [Int]()
+    for n in shortArray {
+        
+        if longArray.contains(n) {
+            let index = longArray.index(of: n)
+            longArray.remove(at: index!)
+            
+            result.append(n)
+        }
+    }
+    
+    return result
+}
+
+let nums1 = [1, 2], nums2 = [1, 1]
+
+print(intersect(nums1, nums2))
