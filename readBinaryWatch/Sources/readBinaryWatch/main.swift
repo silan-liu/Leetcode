12cf82fc9e77092d47719ec52747bcffc1d190d0
silan-liu <413769707@qq.com>
2018-07-31 14:39:30 +0800

    add recusive2

--- readBinaryWatch/Sources/readBinaryWatch/main.swift
+++ readBinaryWatch/Sources/readBinaryWatch/main.swift
@@ -18,7 +18,30 @@ func recursive(_ nums: [Int], _ n: Int, _ result: [Int]) {
     }
 }
 
+func recursive_1(_ nums: [Int], _ n: Int, _ result: inout [Int]) {
+    if n > 0 {
+        if nums.count >= n  {
+            var index = 0
+        
+            for num in nums {
+                // 这里需要用个临时变量，copy
+                var list = result
+                
+                list.append(num)
+                
+                let subNums = Array(nums[index+1..<nums.count])
+                recursive_1(subNums, n - 1, &list)
+                index += 1
+            }
+        }
+    } else {
+        total.append(result)
+    }
+}
+
 let nums = [0, 1, 2, 3]
 var result = [Int]()
-recursive(nums, 2, result)
+//recursive(nums, 2, result)
+recursive_1(nums, 2, &result)
+
 print(total)
