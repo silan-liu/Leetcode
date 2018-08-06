954773f5973b8924e80af14651e355388aaceff7
silan-liu <413769707@qq.com>
2018-08-06 12:22:40 +0800

    add https://leetcode.com/problems/maximum-product-of-three-numbers/description/

--- maximumProduct/Sources/maximumProduct/main.swift
+++ maximumProduct/Sources/maximumProduct/main.swift
@@ -1,1 +1,104 @@
-print("Hello, world!")
+// https://leetcode.com/problems/maximum-product-of-three-numbers/description/
+func maximumProduct(_ nums: [Int]) -> Int {
+    if nums.count <= 2 {
+        return 0
+    }
+    
+    if nums.count == 3 {
+        var result = 1
+        for n in nums {
+            result *= n
+        }
+        
+        return result
+    }
+    
+    var negNums = [Int]()
+    var posNums = [Int]()
+    
+    for n in nums {
+        if n < 0 {
+            negNums.append(n)
+        } else {
+            posNums.append(n)
+        }
+    }
+    
+    // 全为负数
+    if posNums.count == 0 {
+        // 最大的三个数
+        var max1 = Int.min
+        var max2 = Int.min
+        var max3 = Int.min
+        
+        for n in negNums {
+            if n > max1 {
+                max3 = max2
+                max2 = max1
+                max1 = n
+            } else if n > max2 {
+                max3 = max2
+                max2 = n
+            } else if n > max3 {
+                max3 = n
+            }
+        }
+        
+        return max1 * max2 * max3
+    }
+    
+    var p1 = 1
+    if negNums.count >= 2 {
+        // 选出最小的2个数
+        var min1 = Int.max
+        var min2 = Int.max
+        
+        for n in negNums {
+            if n < min1 {
+                min2 = min1
+                min1 = n
+            } else if n < min2 {
+                min2 = n
+            }
+        }
+        
+        p1 = min1 * min2
+    }
+    
+    // 找出最大的三个数
+    var max1 = Int.min
+    var max2 = Int.min
+    var max3 = Int.min
+    
+    var result = 0
+    for n in posNums {
+        if n > max1 {
+            max3 = max2
+            max2 = max1
+            max1 = n
+        } else if n > max2 {
+            max3 = max2
+            max2 = n
+        } else if n > max3 {
+            max3 = n
+        }
+    }
+    
+    // 三个正数的积
+    if posNums.count >= 3 {
+        result = max1 * max2 * max3
+    }
+    
+    // 两个负数和一个正数的积
+    if negNums.count >= 2 {
+        let result1 = max1 * p1
+        if result1 > result {
+            result = result1
+        }
+    }
+    
+    return result
+}
+
+let nums = [0,0,0,4]
+print(maximumProduct(nums))
