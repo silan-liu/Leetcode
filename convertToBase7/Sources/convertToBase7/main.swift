90bb483ba091e4675ae9ed5b6fb4b165ac9653f8
silan-liu <413769707@qq.com>
2018-08-08 19:57:17 +0800

    add https://leetcode.com/problems/base-7/description/

--- convertToBase7/Sources/convertToBase7/main.swift
+++ convertToBase7/Sources/convertToBase7/main.swift
@@ -1,1 +1,29 @@
-print("Hello, world!")
+// https://leetcode.com/problems/base-7/description/
+func convertToBase7(_ num: Int) -> String {
+    var tmp = num
+    var result = ""
+    var bits = [Int]()
+    
+    if num < 0 {
+        tmp = -num
+        result += "-"
+    } else if num == 0 {
+        return "0"
+    }
+    
+    while tmp != 0 {
+        let mod = tmp % 7
+        tmp = tmp / 7
+
+        bits.append(mod)
+    }
+
+    for bit in bits.reversed() {
+        result.append(String(bit))
+    }
+    
+    return result
+}
+
+let num = 0
+print(convertToBase7(num))
