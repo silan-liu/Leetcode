c4cf3d9d6de9e1cbab200aa3dd59d8d71b7a14be
silan-liu <413769707@qq.com>
2018-08-09 21:12:00 +0800

    add https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/

--- maxProfit2/Sources/maxProfit2/main.swift
+++ maxProfit2/Sources/maxProfit2/main.swift
@@ -1,1 +1,28 @@
-print("Hello, world!")
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+func maxProfit(_ prices: [Int]) -> Int {
+    if prices.count < 2 {
+        return 0
+    }
+    
+    var i = 0
+    var max = 0
+    while i < prices.count - 1 {
+        var j = i + 1
+        
+        while j < prices.count {
+            let diff = prices[j] - prices[i]
+            if diff > 0 && diff > max {
+                max = diff
+            }
+            
+            j += 1
+        }
+        
+        i += 1
+    }
+    
+    return max
+ }
+ 
+ let prices = [1,4,2]
+ print(maxProfit(prices))
