9a7b582bf96b9edb66c288fbc16f92810a925ed6
silan-liu <413769707@qq.com>
2018-08-10 15:30:26 +0800

    ad https://leetcode.com/problems/min-cost-climbing-stairs/description/

--- minCostClimbingStairs/Sources/minCostClimbingStairs/main.swift
+++ minCostClimbingStairs/Sources/minCostClimbingStairs/main.swift
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/min-cost-climbing-stairs/description/
 // 递归算法，耗时长
 func minCostClimbingStairs(_ cost: [Int]) -> Int {
     let count = cost.count
@@ -22,5 +23,32 @@ func minCostClimbingStairs(_ cost: [Int]) -> Int {
     }
 }
 
-let cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1, 3, 4, 1, 23, 4,5,6,7,8,3,3,3,4,5,6,78,2,133,3,5]
-print(minCostClimbingStairs(cost))
+func minCostClimbingStairs_2(_ cost: [Int]) -> Int {
+    let count = cost.count
+    if count == 1 {
+        return cost[0]
+    } else if count == 2 {
+        return min(cost[0], cost[1])
+    } else if count > 2 {
+        var f0 = cost[0]
+        var f1 = cost[1]
+        
+        var i = 2
+        while i < count {
+            // 计算到第i步，所需要的最小步数
+            let f = cost[i] + min(f0, f1)
+            f0 = f1
+            f1 = f
+            
+            i += 1
+        }
+        
+        return min(f0, f1)
+    }
+    
+    return 0
+}
+
+let cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1, 3, 4, 1, 23, 4,5,6,7,8,3,3,3,4,5,6,78,2,133,3,5,5,1,4,5,6,7]
+
+print(minCostClimbingStairs_2(cost))
