ad7242b56fe8cba6206e859d44cc8fc8c9b5f07b
silan-liu <413769707@qq.com>
2018-08-23 11:31:59 +0800

    add https://leetcode.com/problems/binary-gap/description/

--- binaryGap/Sources/binaryGap/main.swift
+++ binaryGap/Sources/binaryGap/main.swift
@@ -1,1 +1,31 @@
-print("Hello, world!")
+// https://leetcode.com/problems/binary-gap/description/
+func binaryGap(_ N: Int) -> Int {
+    var value = N
+    var binaryList = [Int]()
+    while value != 0 {
+        let bit = value & 0x1
+        binaryList.insert(bit, at: 0)
+        
+        value >>= 1
+    }
+    
+    var last = -1
+    
+    var i = 0
+    var maxDistance = 0
+    
+    while i < binaryList.count {
+        if binaryList[i] == 1 {
+            if last != -1 {
+                maxDistance = max(maxDistance, i - last)
+            }
+            
+            last = i
+        }
+        i += 1
+    }
+    
+    return maxDistance
+}
+
+print(binaryGap(15))
