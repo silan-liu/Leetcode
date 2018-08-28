2bbfa3100bc33298512a7368e2837b5c72b40dfa
silan-liu <413769707@qq.com>
2018-08-28 14:58:56 +0800

    add https://leetcode.com/problems/fair-candy-swap/description/

--- fairCandySwap/Sources/fairCandySwap/main.swift
+++ fairCandySwap/Sources/fairCandySwap/main.swift
@@ -1,1 +1,28 @@
-print("Hello, world!")
+// https://leetcode.com/problems/fair-candy-swap/description/
+func fairCandySwap(_ A: [Int], _ B: [Int]) -> [Int] {
+    var sa = 0
+    for a in A {
+        sa += a
+    }
+    
+    var sb = 0
+    var map = [Int: Int]()
+    for b in B {
+        sb += b
+        map[b] = 1
+    }
+    
+    let diff = (sb - sa) / 2
+    
+    for a in A {
+        let b = a + diff
+        if let _ = map[b] {
+            return [a, b]
+        }
+    }
+    
+    return []
+}
+
+let A = [1,2,5], B = [2,4]
+print(fairCandySwap(A, B))
