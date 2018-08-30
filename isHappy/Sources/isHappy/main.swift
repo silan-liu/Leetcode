e6656ae0fe4cbd4c3c47c293a5daccf74eda6c9b
silan-liu <413769707@qq.com>
2018-08-30 21:22:43 +0800

    add https://leetcode.com/problems/happy-number/description/

--- isHappy/Sources/isHappy/main.swift
+++ isHappy/Sources/isHappy/main.swift
@@ -1,1 +1,39 @@
-print("Hello, world!")
+// https://leetcode.com/problems/happy-number/description/
+func isHappy(_ n: Int) -> Bool {
+    if n <= 0 {
+        return false
+    }
+    
+    // 如果无限循环，会有重复的数，比如n=2
+    // 4,16,37,58,89,145,42,20,4
+    var set = Set<Int>()
+    var result = caculateSquares(n)
+    while result != 1 {
+        
+        print(result)
+        if set.contains(result) {
+            return false
+        }
+        
+        set.insert(result)
+        result = caculateSquares(result)
+    }
+    
+    return true
+}
+
+func caculateSquares(_ n: Int) -> Int {
+    var tmp = n
+    var result = 0
+    while tmp > 0 {
+        let d = tmp % 10
+        tmp = tmp / 10
+        
+        result += d * d
+    }
+    
+    return result
+}
+
+let n = 2
+print(isHappy(n))
