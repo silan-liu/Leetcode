2ad42bc29ef1c1b377d135711c231d122e300104
silan-liu <413769707@qq.com>
2018-09-06 18:41:51 +0800

    add https://leetcode.com/problems/remove-element/description/

--- removeElement/Sources/removeElement/main.swift
+++ removeElement/Sources/removeElement/main.swift
@@ -1,1 +1,18 @@
-print("Hello, world!")
+// https://leetcode.com/problems/remove-element/description/
+func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+    var i = nums.count - 1
+    while i >= 0 {
+        if nums[i] == val {
+            nums.remove(at: i)
+        }
+        
+        i -= 1
+    }
+    
+    return nums.count
+}
+
+var nums = [3,2,2,3]
+let val = 2
+print(removeElement(&nums, val))
+print(nums)
