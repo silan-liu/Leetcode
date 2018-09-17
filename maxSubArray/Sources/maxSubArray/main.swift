85c33709fa714a096a135bb2cb262053741c3f6a
silan-liu <413769707@qq.com>
2018-09-17 18:23:32 +0800

    add https://leetcode.com/problems/maximum-subarray/description/

--- maxSubArray/Sources/maxSubArray/main.swift
+++ maxSubArray/Sources/maxSubArray/main.swift
@@ -1,1 +1,22 @@
-print("Hello, world!")
+// https://leetcode.com/problems/maximum-subarray/description/
+func maxSubArray(_ nums: [Int]) -> Int {
+    var max = Int.min
+    var sum = 0
+    
+    for num in nums {
+        sum += num
+        
+        if sum > max {
+            max = sum
+        }
+        
+        if sum < 0 {
+            sum = 0
+        }
+    }
+    
+    return max
+}
+
+let nums = [-2,-3,1,2]
+print(maxSubArray(nums))
