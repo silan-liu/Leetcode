9ef60ca0e468d7f681a061ceab8ac9b72100bae0
silan-liu <413769707@qq.com>
2018-09-19 12:29:24 +0800

    https://leetcode.com/problems/convert-a-number-to-hexadecimal/description/

--- toHex/Sources/toHex/main.swift
+++ toHex/Sources/toHex/main.swift
@@ -1,1 +1,33 @@
-print("Hello, world!")
+import Foundation
+
+func toHex(_ num: Int) -> String {
+    var result = ""
+    var map = [10:"a", 11:"b", 12:"c", 13:"d", 14:"e", 15:"f"]
+    
+    let base = 16
+    var tmp = num
+    
+    if tmp < 0 {
+        tmp += 2 << 31
+    } else if tmp == 0 {
+        return "0"
+    }
+    
+    while tmp != 0 {
+        let mod = tmp % base
+        tmp = tmp / base
+        
+        if mod < 10 {
+            result = String(mod) + result
+        } else {
+            if let value = map[mod] {
+                result = value + result
+            }
+        }
+    }
+    
+    return result
+}
+
+let num = -1
+print(toHex(num))
