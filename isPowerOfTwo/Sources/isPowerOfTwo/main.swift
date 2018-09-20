a8a1acf69f8572ebd6f1664fa5f240a403f739d5
silan-liu <413769707@qq.com>
2018-09-20 09:55:59 +0800

    https://leetcode.com/problems/power-of-two/description/

--- isPowerOfTwo/Sources/isPowerOfTwo/main.swift
+++ isPowerOfTwo/Sources/isPowerOfTwo/main.swift
@@ -1,1 +1,26 @@
-print("Hello, world!")
+// https://leetcode.com/problems/power-of-two/description/
+func isPowerOfTwo(_ n: Int) -> Bool {
+    if n == 0 {
+        return false
+    }
+    
+    if n == 1 {
+        return true
+    }
+    
+    var tmp = n
+    while tmp != 1 {
+        let mod = tmp % 2
+        
+        if mod != 0 {
+            return false
+        }
+        
+        tmp = tmp / 2
+    }
+    
+    return true
+}
+
+
+print(isPowerOfTwo(1024))
