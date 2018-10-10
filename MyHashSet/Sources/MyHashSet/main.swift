25432a2ccf8320cef3e32bff9fd2850abdc7e1a2
silan-liu <413769707@qq.com>
2018-10-10 21:21:30 +0800

    add https://leetcode.com/problems/design-hashset/description/

--- MyHashSet/Sources/MyHashSet/main.swift
+++ MyHashSet/Sources/MyHashSet/main.swift
@@ -1,1 +1,51 @@
-print("Hello, world!")
+
+class MyHashSet {
+    var list: [Int]
+    
+    /** Initialize your data structure here. */
+    init() {
+        list = [Int]()
+    }
+    
+    func add(_ key: Int) {
+        if self.contains(key) {
+            return
+        }
+        
+        list.append(key)
+    }
+    
+    func remove(_ key: Int) {
+        var i = 0
+        while i < list.count {
+            if list[i] == key {
+                list.remove(at: i)
+                break
+            }
+            i += 1
+        }
+    }
+    
+    /** Returns true if this set contains the specified element */
+    func contains(_ key: Int) -> Bool {
+        var i = 0
+        while i < list.count {
+            if list[i] == key {
+                return true
+            }
+            i += 1
+        }
+        
+        return false
+    }
+}
+
+let hashSet = MyHashSet()
+hashSet.add(1);
+hashSet.add(2);
+print(hashSet.contains(1))
+print(hashSet.contains(3))
+hashSet.add(2);
+print(hashSet.contains(2))
+hashSet.remove(2);
+print(hashSet.contains(2))
