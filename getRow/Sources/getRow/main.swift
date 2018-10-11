4ee09643759ae9510a8e8d85758d79f7e66f2191
silan-liu <413769707@qq.com>
2018-10-11 09:00:06 +0800

    add https://leetcode.com/problems/pascals-triangle-ii/description/

--- getRow/Sources/getRow/main.swift
+++ getRow/Sources/getRow/main.swift
@@ -1,1 +1,37 @@
-print("Hello, world!")
+// https://leetcode.com/problems/pascals-triangle-ii/description/
+func getRow(_ rowIndex: Int) -> [Int] {
+    if rowIndex == 0 {
+        return [1]
+    } else if rowIndex == 1 {
+        return [1, 1]
+    } else if rowIndex >= 2 {
+        var list = [1,1]
+        var i = 2
+        while i <= rowIndex {
+            var j = 0
+            var result = [Int]()
+            while j <= rowIndex {
+                if j == 0 || j == rowIndex {
+                    result.append(1)
+                } else {
+                    if j - 1 >= 0 && j < list.count {
+                        let value = list[j - 1] + list[j]
+                        result.append(value)
+                    }
+                }
+                
+                j += 1
+            }
+            
+            list = result
+            
+            i += 1
+        }
+        
+        return list
+    } else {
+        return [1]
+    }
+}
+
+print(getRow(9))
