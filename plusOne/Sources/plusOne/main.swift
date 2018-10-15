06569c5713c2f0891ce19d939099a032943df37a
silan-liu <413769707@qq.com>
2018-10-15 15:12:08 +0800

    add https://leetcode.com/problems/plus-one/description/

--- plusOne/Sources/plusOne/main.swift
+++ plusOne/Sources/plusOne/main.swift
@@ -1,1 +1,30 @@
-print("Hello, world!")
+// https://leetcode.com/problems/plus-one/description/
+func plusOne(_ digits: [Int]) -> [Int] {
+    var c = 0
+    var result = digits
+    
+    var i = result.count - 1
+    while i >= 0 {
+        var value = digits[i]
+        var newValue = value + c
+        if i == result.count - 1 {
+            newValue = value + 1
+        }
+        
+        value = newValue % 10
+        c = newValue / 10
+        
+        result[i] = value
+        i -= 1
+    }
+    
+    if c > 0 {
+        result.insert(c, at: 0)
+    }
+    
+    return result
+}
+
+
+let digits = [0,0,9]
+print(plusOne(digits))
