8abf687a717ebd22938bcb8fbd2845f39c77abbc
silan-liu <413769707@qq.com>
2018-10-23 11:05:17 +0800

    add https://leetcode.com/problems/valid-perfect-square/submissions/

--- isPerfectSquare/Sources/isPerfectSquare/main.swift
+++ isPerfectSquare/Sources/isPerfectSquare/main.swift
@@ -1,1 +1,17 @@
-print("Hello, world!")
+ func isPerfectSquare(_ num: Int) -> Bool {
+    var i = 1
+    while i <= num / 2 {
+        let value = i * i
+        if value == num {
+            return true
+        } else if value > num {
+            return false
+        }
+        
+        i += 1
+    }
+    
+    return false
+ }
+ 
+ print(isPerfectSquare(4000000))
