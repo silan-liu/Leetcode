b6813f94464d3eaf3abd937ce57c968bca129c0b
silan-liu <413769707@qq.com>
2018-10-26 09:23:40 +0800

    add https://leetcode.com/problems/power-of-four/

--- isPowerOfFour/Sources/isPowerOfFour/main.swift
+++ isPowerOfFour/Sources/isPowerOfFour/main.swift
@@ -1,1 +1,26 @@
-print("Hello, world!")
+class Solution {
+    func isPowerOfFour(_ num: Int) -> Bool {
+        if num == 0 {
+            return false
+        }
+        
+        if num == 1 {
+            return true
+        }
+        
+        var tmp = num
+        while tmp != 1 {
+            if tmp % 4 != 0 {
+                return false
+            }
+            
+            tmp = tmp / 4
+        }
+        
+        return true
+    }
+}
+
+let s = Solution()
+print(s.isPowerOfFour(64))
+print(s.isPowerOfFour(6))
