395bb4b859b610e369a42fc9c4b4a90cedebb363
silan-liu <413769707@qq.com>
2018-10-26 09:58:20 +0800

    add nice solution

--- isBalanced/Sources/isBalanced/main.swift
+++ isBalanced/Sources/isBalanced/main.swift
@@ -37,6 +37,41 @@ func height(_ root: TreeNode?) -> Int {
     }
 }
 
+// nice solution
+func isBalanced_2(_ root: TreeNode?) -> Bool {
+    if let root = root {
+        return height_2(root) != -1
+    }
+    
+    return true
+}
+// 在计算高度的过程中就进行比较
+func height_2(_ root: TreeNode?) -> Int {
+    if let root = root {
+        let leftHeight = height_2(root.left)
+        // 已经不平衡
+        if leftHeight == -1 {
+            return -1
+        } else {
+            let rightHeight = height_2(root.right)
+            
+            // 已经不平衡
+            if rightHeight == -1 {
+                return -1
+            } else {
+                // 不平衡
+                if abs(leftHeight - rightHeight) <= 1 {
+                    return 1 + max(leftHeight, rightHeight)
+                }
+                
+                return -1
+            }
+        }
+    } else {
+        return 0
+    }
+}
+
 let root = TreeNode(3)
 let left = TreeNode(9)
 let right = TreeNode(20)
@@ -47,3 +82,4 @@ right.left = TreeNode(15)
 right.right = TreeNode(7)
 
 print(isBalanced(root))
+print(isBalanced_2(root))
