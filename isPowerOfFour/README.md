d8af700db42f5d14289e0663280bd7b48cb68fd7
silan-liu <413769707@qq.com>
2018-10-26 09:22:21 +0800

    init

---
+++ isPowerOfFour/README.md
@@ -0,0 +1,3 @@
+# isPowerOfFour
+
+A description of this package.
---
+++ isPowerOfFour/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import isPowerOfFourTests
+
+var tests = [XCTestCaseEntry]()
+tests += isPowerOfFourTests.allTests()
+XCTMain(tests)
