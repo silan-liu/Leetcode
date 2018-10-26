4ff19352e68d5151a199fd3ae6c4427833ef12d7
silan-liu <413769707@qq.com>
2018-10-26 09:42:51 +0800

    add https://leetcode.com/problems/balanced-binary-tree/

--- isBalanced/Sources/isBalanced/main.swift
+++ isBalanced/Sources/isBalanced/main.swift
@@ -1,1 +1,49 @@
-print("Hello, world!")
+// https://leetcode.com/problems/balanced-binary-tree/
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.left = nil
+        self.right = nil
+    }
+ }
+ 
+
+func isBalanced(_ root: TreeNode?) -> Bool {
+    if let root = root {
+        if root.left == nil && root.right == nil {
+            return true
+        }
+        
+        let leftHeight = height(root.left)
+        let rightHeight = height(root.right)
+        if abs(leftHeight - rightHeight) > 1 {
+            return false
+        }
+        
+        return isBalanced(root.left) && isBalanced(root.right)
+    }
+    
+    return true
+}
+
+func height(_ root: TreeNode?) -> Int {
+    if let root = root {
+        return 1 + max(height(root.left), height(root.right))
+    } else {
+        return 0
+    }
+}
+
+let root = TreeNode(3)
+let left = TreeNode(9)
+let right = TreeNode(20)
+root.left = left
+root.right = right
+
+right.left = TreeNode(15)
+right.right = TreeNode(7)
+
+print(isBalanced(root))
