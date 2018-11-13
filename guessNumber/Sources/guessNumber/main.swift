5e8e5c0a601b753dd7840ea4bb296a9da8fd4d14
silan-liu <413769707@qq.com>
2018-11-13 10:10:24 +0800

    add https://leetcode.com/problems/guess-number-higher-or-lower/

--- guessNumber/Sources/guessNumber/main.swift
+++ guessNumber/Sources/guessNumber/main.swift
@@ -1,1 +1,35 @@
-print("Hello, world!")
+// https://leetcode.com/problems/guess-number-higher-or-lower/
+var pick = 6
+func guessNumber(_ n: Int, _ pick: Int) -> Int {
+    var low = 1
+    var high = n
+    
+    while low <= high {
+        let mid = low + (high - low) / 2
+        
+        let result = guess(mid)
+        if result == 0 {
+            return mid
+        } else if result == -1 {
+            // lower
+            low = mid + 1
+        } else if result == 1 {
+            // higher
+            high = mid - 1
+        }
+    }
+    
+    return -1
+}
+
+func guess(_ num: Int) -> Int {
+    if num < pick {
+        return -1
+    } else if num == pick {
+        return 0
+    } else {
+        return 1
+    }
+}
+
+print(guessNumber(10, pick))
