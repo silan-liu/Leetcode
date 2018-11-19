e193b100ff48c10ab80f6ef7483c96f9b43ce90a
silan-liu <413769707@qq.com>
2018-11-19 17:02:38 +0800

    add comment

--- trailingZeroes/Sources/trailingZeroes/main.swift
+++ trailingZeroes/Sources/trailingZeroes/main.swift
@@ -1,7 +1,7 @@
 // https://leetcode.com/problems/factorial-trailing-zeroes/submissions/
 // 最主要找到因子5的个数，2的个数不用担心，能凑够。
 // 如果n = 25,能被5整除的数为25/5=5,这一轮中因子5的个数为5。(5,10,15,20,25)
-// 而由于25可以分解成5*5，所以其实还多一个。
+// 而由于25可以分解成5*5，其实还多一个5，所以还需算上被25整除的数。依次类推，需要不断的算上能被5^x次方整除的数。
 func trailingZeroes(_ n: Int) -> Int {
     var factor = 5
     var count = 0
