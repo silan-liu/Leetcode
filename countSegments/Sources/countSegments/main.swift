4455c7f17c16c34554afca69f3bc1373a4d72e52
silan-liu <413769707@qq.com>
2018-11-27 11:05:15 +0800

    add https://leetcode.com/problems/number-of-segments-in-a-string/submissions/

--- countSegments/Sources/countSegments/main.swift
+++ countSegments/Sources/countSegments/main.swift
@@ -3,3 +3,32 @@ func countSegments(_ s: String) -> Int {
     let substring = s.split(separator: " ")
     return substring.count
 }
+
+// 逐个遍历
+func countSegments_2(_ s: String) -> Int {
+    var count = 0
+    var flag = false
+    var i = 0
+    for c in s {
+        if c == " " {
+            if flag {
+                count += 1
+                flag = false
+            }
+        } else {
+            flag = true
+            
+            if i == s.count - 1 {
+                count += 1
+            }
+        }
+        
+        i += 1
+    }
+    
+    return count
+}
+
+let s = "hello, world kkk kk   what, m"
+print(countSegments(s))
+print(countSegments_2(s))
