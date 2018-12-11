32236c36e380f626e6e80c45410028266b8ed9d1
silan-liu <413769707@qq.com>
2018-12-11 11:03:19 +0800

    add solution2

--- LinkListIsPalindrome/Sources/LinkListIsPalindrome/main.swift
+++ LinkListIsPalindrome/Sources/LinkListIsPalindrome/main.swift
@@ -23,12 +23,12 @@ class Solution {
                 p2 = p2?.next?.next
             }
             
-            // p1是后半部分
+            // 无论链表长度为偶数还是奇数，都为n/2的节点；当为奇数，则正好是中间的节点。
             if p1 == nil {
                 return false
             }
             
-            // 这里不能用字符串来比较，因为如果是负数，反转后不相等
+            // 这里不能用字符串来比较，因为如果是负数，反转后不相等，但是空间复杂度为O(n)
             var list1 = [Int]()
             var list2 = [Int]()
             
@@ -55,18 +55,65 @@ class Solution {
         
         return true
     }
+    
+    // 第二种方式，反转前半段链表
+    func isPalindrome_2(_ head: ListNode?) -> Bool {
+        // 通过快慢指针找到中间节点的下一节点，正好是后半部分的开始
+        if let head = head {
+            
+            var p1: ListNode? = head
+            var p2: ListNode? = head
+            var last: ListNode? = nil
+            var current: ListNode? = head
+            
+            while p2 != nil, p2?.next != nil {
+                current = p1
+                p1 = p1?.next
+                p2 = p2?.next?.next
+                
+                // 改变指向，逐步反转
+                current?.next = last
+                last = current
+            }
+            
+            // p1是后半部分
+            // 如果链表为奇数，p1需要往后移动一个节点，即中间节点的后一个。
+            if p2 != nil, p2?.next == nil {
+                p1 = p1?.next
+            }
+            
+            // p2是前半部分反转链表的头
+            p2 = current
+            
+            while p1 != nil {
+                if p1?.val != p2?.val {
+                    return false
+                }
+                p1 = p1?.next
+                p2 = p2?.next
+            }
+            
+            return true
+        }
+        
+        return true
+    }
+    
+    // 第三种方式，遍历链表，用栈存储元素的值，然后从头开始遍历链表，对比栈顶元素。
+    
 }
 
-let node1 = ListNode(-129)
-let node2 = ListNode(-129)
+let node1 = ListNode(1)
+let node2 = ListNode(3)
 let node3 = ListNode(1)
-let node4 = ListNode(2)
+let node4 = ListNode(3)
 let node5 = ListNode(1)
 
 node1.next = node2
-//node2.next = node3
+node2.next = node3
 //node3.next = node4
 //node4.next = node5
 
 let obj = Solution()
 print(obj.isPalindrome(node1))
+print(obj.isPalindrome_2(node1))
