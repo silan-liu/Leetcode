488bb6945874458b560be4a0bda7f13ecaa735f8
silan-liu <413769707@qq.com>
2018-12-12 09:55:57 +0800

    add solution2

--- validMountainArray/Sources/validMountainArray/main.swift
+++ validMountainArray/Sources/validMountainArray/main.swift
@@ -37,5 +37,28 @@ func validMountainArray(_ A: [Int]) -> Bool {
     return index != 0 && index != A.count - 1
 }
 
+func validMountainArray_2(_ A: [Int]) -> Bool {
+    if A.count < 3 {
+        return false
+    }
+    
+    var i = 0
+    while i + 1 < A.count, A[i] < A[i+1] {
+        i += 1
+    }
+    
+    if i == 0 || i == A.count - 1 {
+        return false
+    }
+    
+    // 最高点
+    while i + 1 < A.count, A[i] > A[i+1] {
+        i += 1
+    }
+    
+    return i == A.count - 1
+}
+
 let nums = [5,4,3,2,1]
 print(validMountainArray(nums))
+print(validMountainArray_2(nums))
