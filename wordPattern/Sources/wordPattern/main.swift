69b26a48e28e540e7fe2b1a5e4bf698680b640e5
silan-liu <413769707@qq.com>
2018-12-17 09:52:37 +0800

    add faster solution

--- wordPattern/Sources/wordPattern/main.swift
+++ wordPattern/Sources/wordPattern/main.swift
@@ -34,5 +34,43 @@
     return true
  }
  
+ // faster
+ func wordPattern_2(_ pattern: String, _ str: String) -> Bool {
+    let wordList = str.split(separator: " ")
+    let patternList = Array(pattern)
+    
+    if patternList.count != wordList.count {
+        return false
+    }
+    
+    var i = 0
+    var map = [Character: String]()
+    // 记录已经被映射过的
+    var set = Set<String>()
+    
+    while i < patternList.count {
+        let key = patternList[i]
+        let word = String(wordList[i])
+        
+        if let value = map[key] {
+            if word != value {
+                return false
+            }
+        } else {
+            if set.contains(word) {
+                return false
+            }
+            
+            map[key] = word
+            set.insert(word)
+        }
+        
+        i += 1
+    }
+    
+    return true
+ }
+ 
  let pattern = "abba", str = "dog dog dog dog"
  print(wordPattern(pattern, str))
+ print(wordPattern_2(pattern, str))
