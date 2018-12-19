859cbf300c106b0b50f846b21d0c302a2daf4eb6
silan-liu <413769707@qq.com>
2018-12-19 10:48:32 +0800

    add

--- merge/Sources/merge/main.swift
+++ merge/Sources/merge/main.swift
@@ -42,8 +42,40 @@ func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
     }
 }
 
-var nums1 = [1,2,3,0,0,0], m = 1
+// 更优，从后往前比，这样不会覆盖nums1的前m个数据
+func merge_2(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+    if m > nums1.count || n > nums2.count || m + n > nums1.count {
+        return
+    }
+
+    var i = m - 1
+    var j = n - 1
+    var k = m + n - 1
+
+    while i >= 0, j >= 0 {
+        if nums1[i] > nums2[j] {
+            nums1[k] = nums1[i]
+            i -= 1
+        } else {
+            nums1[k] = nums2[j]
+            j -= 1
+        }
+
+        k -= 1
+    }
+
+    // 如果nums2还未遍历完
+    while j >= 0 {
+        nums1[k] = nums2[j]
+        k -= 1
+        j -= 1
+    }
+}
+
+var nums1 = [1,2,3,0,0,0], m = 3
 let nums2 = [2,5,6],       n = 3
 
-merge(&nums1, m, nums2, n)
+// merge(&nums1, m, nums2, n)
+merge_2(&nums1, m, nums2, n)
+
 print(nums1)
