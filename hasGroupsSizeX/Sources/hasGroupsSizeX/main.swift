6703b0ff9795de615c2a331cc233d6ab30e794ae
silan-liu <413769707@qq.com>
2018-12-21 11:30:10 +0800

    add solution

--- hasGroupsSizeX/Sources/hasGroupsSizeX/main.swift
+++ hasGroupsSizeX/Sources/hasGroupsSizeX/main.swift
@@ -42,5 +42,41 @@ func hasGroupsSizeX(_ deck: [Int]) -> Bool {
     return false
 }
 
+// 求最大公约数，如果公约数>=2，则满足。
+// greatest common divisor
+func hasGroupsSizeX_2(_ deck: [Int]) -> Bool {
+    if deck.count <= 1 {
+        return false
+    }
+
+    var map = [Int: Int]()
+
+    for n in deck {
+        var count = map[n]
+        if count == nil {
+            count = 1
+        } else {
+            count! += 1
+        }
+        map[n] = count!
+    }
+
+    var count = -1
+    for (_, value) in map {
+        if count == -1 {
+            count = value
+        } else {
+            count = gcd(count, value)
+        }
+    }
+
+    return count >= 2
+}
+
+func gcd(_ x: Int, _ y: Int) -> Int {
+    return x == 0 ? y : gcd(y % x, y)
+}
+
 let deck = [1,1,2,2,2,2]
 print(hasGroupsSizeX(deck))
+print(hasGroupsSizeX_2(deck))
