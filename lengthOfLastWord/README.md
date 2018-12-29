ac4c77421fcb62076182c7e898e471043906c6c7
silan-liu <413769707@qq.com>
2018-12-29 09:46:40 +0800

    init

---
+++ lengthOfLastWord/README.md
@@ -0,0 +1,3 @@
+# lengthOfLastWord
+
+A description of this package.
---
+++ lengthOfLastWord/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import lengthOfLastWordTests
+
+var tests = [XCTestCaseEntry]()
+tests += lengthOfLastWordTests.allTests()
+XCTMain(tests)
