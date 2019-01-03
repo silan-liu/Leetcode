8d3fce05b9861b411a331a6225720656b0cab8bf
silan-liu <413769707@qq.com>
2019-01-03 19:09:53 +0800

    init

---
+++ getIntersectionNode/getIntersectionNode.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+     int val;
+     struct ListNode *next;
+};
+
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) {
+    
+}
+
+int main() {
+    printf("%d", 2);
+    return 0;
+}
