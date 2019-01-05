8c0e2ea2633d7130a826cb267188a0cf96b4601a
silan-liu <413769707@qq.com>
2019-01-06 00:00:36 +0800

    add solution

--- reachNumber/Sources/reachNumber/main.swift
+++ reachNumber/Sources/reachNumber/main.swift
@@ -18,7 +18,7 @@ func reachNumber(_ target: Int) -> Int {
             // +变-，需要减去2倍
             if delta % 2 == 0 {
                 let n = delta / 2
-                // 要减去的数
+                // 要减去的数, 1..i之和为i * (i + 1) / 2，为正数时，n肯定在范围内，其实不用判断
                 if n <= i * (i + 1) / 2 {
                     return i
                 } else {
@@ -31,5 +31,30 @@ func reachNumber(_ target: Int) -> Int {
     }
 }
 
+func reachNumber_2(_ target: Int) -> Int {
+    let value = abs(target)
+
+    var i = 0
+    var sum = 0
+    while true {
+        sum += i
+        if sum == value {
+            return i
+        } else if sum < value {
+            i += 1
+        } else {
+            // 计算要减的数
+            let delta = sum - value
+            // +变-，需要减去2倍
+            if delta % 2 == 0 {
+                return i
+            } else {
+                i += 1
+            }
+        }
+    }
+}
+
 let target = -5
 print(reachNumber(target))
+print(reachNumber_2(target))
