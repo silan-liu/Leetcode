783b172a37c298b33f2e34a6e667adaf8a8681d9
silan-liu <413769707@qq.com>
2019-01-09 09:49:52 +0800

    add https://leetcode.com/problems/sqrtx/submissions/

--- mySqrt/Sources/mySqrt/main.swift
+++ mySqrt/Sources/mySqrt/main.swift
@@ -1,1 +1,18 @@
-print("Hello, world!")
+// https://leetcode.com/problems/sqrtx/submissions/
+func mySqrt(_ x: Int) -> Int {
+    var i = 0
+    while true {
+        let multiply = i * i
+        if multiply == x {
+            return i
+        } else if multiply < x {
+            if (i + 1) * (i + 1) > x {
+                return i
+            }
+        }
+
+        i += 1
+    }
+}
+
+print(mySqrt(19))
