bfc62f436a65498331d31b15b5c242733e1b274c
silan-liu <413769707@qq.com>
2019-01-11 20:30:31 +0800

    add https://leetcode.com/problems/valid-palindrome/submissions/

--- isPalindrome2/Sources/isPalindrome2/main.swift
+++ isPalindrome2/Sources/isPalindrome2/main.swift
@@ -1,1 +1,39 @@
-print("Hello, world!")
+// https://leetcode.com/problems/valid-palindrome/submissions/
+func isPalindrome(_ s: String) -> Bool {
+    let list = Array(s)
+    var i = 0
+    var j = list.count - 1
+    while i <= j {
+        let c1 = String(list[i]).lowercased()
+        let c2 = String(list[j]).lowercased()
+        
+        let f1 = isAlphaNumberic(c1)
+        let f2 = isAlphaNumberic(c2)
+        
+        if f1 && f2 {
+            if c1 == c2 {
+                i += 1
+                j -= 1
+            } else {
+                return false
+            }
+        } else if !f1 {
+            i += 1
+        } else {
+            j -= 1
+        }
+    }
+
+    return true
+}
+
+func isAlphaNumberic(_ char: String) -> Bool {
+    if (char >= "0" && char <= "9") || (char >= "a" && char <= "z") {
+        return true
+    }
+
+    return false
+}
+
+let s = "aab1ba,ac"
+print(isPalindrome(s))
