32b9196e4a112034e59f491df68f04dd0e775a9a
silan-liu <413769707@qq.com>
2019-01-14 19:35:46 +0800

    add https://leetcode.com/problems/walking-robot-simulation/submissions/

--- robotSim/Sources/robotSim/main.swift
+++ robotSim/Sources/robotSim/main.swift
@@ -1,7 +1,142 @@
+// https://leetcode.com/problems/walking-robot-simulation/submissions/
+class Solution {
+    var map = [String: [Int]]()
+    
+    func robotSim(_ commands: [Int], _ obstacles: [[Int]]) -> Int {
+        var x = 0
+        var y = 0
+        var max = Int.min
+
+        // 处理obstacle
+        for obstacle in obstacles {
+            if obstacle.count == 2 {
+                var key = "x_" + String(obstacle[0])
+                var value = map[key]
+                if value == nil {
+                    value = [Int]()
+                }
+                
+                value?.append(obstacle[1])
+                map[key] = value
+                
+                key = "y_" + String(obstacle[1])
+                value = map[key]
+                if value == nil {
+                    value = [Int]()
+                }
+                
+                value?.append(obstacle[0])
+                map[key] = value
+            }
+        }
+
+        // 0-up, 2-down, 3-left, 1-right
+        var direction = 0
+        for cmd in commands {
+            // turn left 90 degree
+            if cmd == -2 {
+                direction = (direction + 3) % 4
+            } else if cmd == -1 {
+                // tuen right 90 degree
+                direction = (direction + 1) % 4
+            } else if cmd >= 1 && cmd <= 9 {
+                // move forward
+                handleObstacle(&x, &y, direction, cmd, obstacles)
+                print(x,y)
+                let result = x * x + y * y
+                if max < result {
+                    max = result
+                }
+            }
+        }
+        
+        return max
+    }
+    
+    func handleObstacle(_ x : inout Int, _ y  : inout Int, _ direction: Int, _ cmd: Int, _ obstacles: [[Int]]) {
+        
+        let xKey = "x_" + String(x)
+        let yKey = "y_" + String(y)
+
+        if direction == 0 {
+            // up
+            let tmp = y
+            y += cmd
+            var max = Int.min
+
+            if let value = map[xKey], value.count > 0 {
+                for v in value {
+                    if tmp < v, y >= v {
+                        let d = y - v
+                        if d > max {
+                            max = d
+                            y = v - 1
+                        }
+                    }
+                }
+            }
+        } else if direction == 1 {
+            // r
+            let tmp = x
+            x += cmd
+            var max = Int.min
+            if let value = map[yKey], value.count > 0 {
+                for v in value {
+                    if tmp < v, x >= v {
+                        let d = x - v
+                        if d > max {
+                            max = d
+                            x = v - 1
+                        }
+                    }
+                }
+            }
+            
+        } else if direction == 2 {
+            // d
+            let tmp = y
+            y -= cmd
+            var min = Int.max
+            if let value = map[xKey], value.count > 0 {
+                for v in value {
+                    if tmp > v, y <= v {
+                        let d = y - v
+                        if d < min {
+                            min = d
+                            y = v + 1
+                        }
+                    }
+                }
+            }
+
+        } else if direction == 3 {
+            // l
+            let tmp = x
+            x -= cmd
+            
+            var min = Int.max
+            
+            if let value = map[yKey], value.count > 0 {
+                for v in value {
+                    if tmp > v, x <= v {
+                        let d = x - v
+                        if d < min {
+                            min = d
+                            x = v + 1
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+// time limited exceed
 func robotSim(_ commands: [Int], _ obstacles: [[Int]]) -> Int {
     var x = 0
     var y = 0
-
+    var max = Int.min
+    
     // 0-up, 2-down, 3-left, 1-right
     var direction = 0
     for cmd in commands {
@@ -14,28 +149,30 @@ func robotSim(_ commands: [Int], _ obstacles: [[Int]]) -> Int {
         } else if cmd >= 1 && cmd <= 9 {
             // move forward
             beforeObstacle(&x, &y, direction, cmd, obstacles)
+
+            let result = x * x + y * y
+            if max < result {
+                max = result
+            }
         }
     }
 
-    return x * x + y * y
+    return max
 }
 
 func beforeObstacle(_ x : inout Int, _ y  : inout Int, _ direction: Int, _ cmd: Int, _ obstacles: [[Int]]) {
-    if obstacles.count == 0 {
-        return
-    }
-
+ 
     if direction == 0 {
         // up
+        let tmp = y
         y += cmd
-
-        var min = Int.max
+        var max = Int.min
         for obstacle in obstacles {
             if obstacle.count == 2 {
-                if y >= obstacle[1] {
+                if x == obstacle[0], tmp < obstacle[1], y >= obstacle[1] {
                     let d = y - obstacle[1]
-                    if min < d {
-                        min = d
+                    if d > max {
+                        max = d
                         y = obstacle[1] - 1
                     }
                 }
@@ -43,14 +180,15 @@ func beforeObstacle(_ x : inout Int, _ y  : inout Int, _ direction: Int, _ cmd: 
         }
     } else if direction == 1 {
         // r
+        let tmp = x
         x += cmd
-        var min = Int.max
+        var max = Int.min
         for obstacle in obstacles {
             if obstacle.count == 2 {
-                if x >= obstacle[0] {
+                if y == obstacle[1], tmp < obstacle[0], x >= obstacle[0] {
                     let d = x - obstacle[0]
-                    if min < d {
-                        min = d
+                    if d > max {
+                        max = d
                         x = obstacle[0] - 1
                     }
                 }
@@ -58,13 +196,14 @@ func beforeObstacle(_ x : inout Int, _ y  : inout Int, _ direction: Int, _ cmd: 
         }
     } else if direction == 2 {
         // d
+        let tmp = y
         y -= cmd
         var min = Int.max
         for obstacle in obstacles {
             if obstacle.count == 2 {
-                if y <= obstacle[1] {
+                if x == obstacle[0], tmp > obstacle[1], y <= obstacle[1] {
                     let d = y - obstacle[1]
-                    if min < d {
+                    if d < min {
                         min = d
                         y = obstacle[1] + 1
                     }
@@ -73,14 +212,15 @@ func beforeObstacle(_ x : inout Int, _ y  : inout Int, _ direction: Int, _ cmd: 
         }
     } else if direction == 3 {
         // l
+        let tmp = x
         x -= cmd
 
         var min = Int.max
         for obstacle in obstacles {
             if obstacle.count == 2 {
-                if x <= obstacle[0] {
+                if y == obstacle[1], tmp > obstacle[0], x <= obstacle[0] {
                     let d = x - obstacle[0]
-                    if min < d {
+                    if d < min {
                         min = d
                         x = obstacle[0] + 1
                     }
@@ -90,6 +230,13 @@ func beforeObstacle(_ x : inout Int, _ y  : inout Int, _ direction: Int, _ cmd: 
     }
 }
 
-let commands = [4,-1,3], obstacles = [[Int]]()
+
+let commands = [4,-1,4,-2,4]
+
+let obstacles = [[2,4]]
 let result = robotSim(commands, obstacles)
 print(result)
+
+let s = Solution()
+let r = s.robotSim(commands, obstacles)
+print(r)
