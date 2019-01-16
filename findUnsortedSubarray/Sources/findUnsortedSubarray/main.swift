b5f062254699de926d5eb4ceb9a77fe431cad693
silan-liu <413769707@qq.com>
2019-01-16 11:40:19 +0800

    add https://leetcode.com/problems/shortest-unsorted-continuous-subarray/submissions/

--- findUnsortedSubarray/Sources/findUnsortedSubarray/main.swift
+++ findUnsortedSubarray/Sources/findUnsortedSubarray/main.swift
@@ -1,1 +1,40 @@
-print("Hello, world!")
+// https://leetcode.com/problems/shortest-unsorted-continuous-subarray/submissions/
+class Solution {
+    func findUnsortedSubarray(_ nums: [Int]) -> Int {
+        let sortedArray = nums.sorted()
+        var i = 0
+        var j = nums.count - 1
+
+        var find_start = false
+        var find_end = false
+        var start = 0
+        var end = 0
+
+        while i <= j {
+            if sortedArray[i] != nums[i] {
+                start = i
+                find_start = true
+            } else {
+                i += 1
+            }
+
+            if sortedArray[j] != nums[j] {
+                end = j
+                find_end = true
+            } else {
+                j -= 1
+            }
+
+            if find_start && find_end {
+                return end - start + 1
+            }
+        }
+
+        return 0
+    }
+}
+
+let s = Solution()
+let nums = [7,8,9,2,3,4,10]
+print(s.findUnsortedSubarray(nums))
+
