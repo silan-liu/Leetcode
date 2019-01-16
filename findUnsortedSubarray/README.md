1b21ea2b6f23e99230e1c4cc5a64b24736777437
silan-liu <413769707@qq.com>
2019-01-16 10:33:07 +0800

    init

---
+++ findUnsortedSubarray/README.md
@@ -0,0 +1,3 @@
+# findUnsortedSubarray
+
+A description of this package.
---
+++ findUnsortedSubarray/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import findUnsortedSubarrayTests
+
+var tests = [XCTestCaseEntry]()
+tests += findUnsortedSubarrayTests.allTests()
+XCTMain(tests)
