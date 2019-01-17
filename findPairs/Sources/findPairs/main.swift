9d600fa3d74eccb3f167d575f541188695743561
silan-liu <413769707@qq.com>
2019-01-17 10:09:13 +0800

    add https://leetcode.com/problems/k-diff-pairs-in-an-array/submissions/

--- findPairs/Sources/findPairs/main.swift
+++ findPairs/Sources/findPairs/main.swift
@@ -1,1 +1,35 @@
-print("Hello, world!")
+// https://leetcode.com/problems/k-diff-pairs-in-an-array/submissions/
+func findPairs(_ nums: [Int], _ k: Int) -> Int {
+    if k < 0 {
+        return 0
+    }
+    var map = [Int: Int]()
+
+    for n in nums {
+        var c = 1
+        if let value = map[n] {
+            c = value + 1
+        }
+
+        map[n] = c
+    }
+
+    var count = 0
+    for (key, value) in map {
+        // 需要判断个数是否>=2
+        if k == 0 {
+            if value >= 2 {
+                count += 1
+            } 
+        } else {
+            if let _ = map[key + k] {
+                count += 1
+            }
+        }
+    }
+
+    return count
+}
+
+let nums = [1, 1, 2, 2], k = -1
+print(findPairs(nums, k))
