955ca03faf604819ae3adbcfde4387f6825d3007
silan-liu <413769707@qq.com>
2019-01-20 11:10:49 +0800

    init

---
+++ convertToTitle/README.md
@@ -0,0 +1,3 @@
+# convertToTitle
+
+A description of this package.
---
+++ convertToTitle/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import convertToTitleTests
+
+var tests = [XCTestCaseEntry]()
+tests += convertToTitleTests.allTests()
+XCTMain(tests)
