6057d0007dd5dcd21033a5b82e0aa20cc7a9b815
silan-liu <413769707@qq.com>
2019-01-22 16:52:59 +0800

    add solution (time limited exceed)

--- countPrimes/Sources/countPrimes/main.swift
+++ countPrimes/Sources/countPrimes/main.swift
@@ -1,1 +1,32 @@
-print("Hello, world!")
+func countPrimes(_ n: Int) -> Int {
+    var i = 2
+    var count = 0
+    while i < n {
+        if isPrime(i) {
+            count += 1
+        }
+
+        i += 1
+    }
+
+    return count
+}
+
+func isPrime(_ n: Int) -> Bool {
+    if n <= 1 {
+        return false
+    }
+
+    var i = 2
+    while i <= n / 2 {
+        if n % i == 0 {
+            return false
+        }
+
+        i += 1
+    }
+
+    return true
+}
+
+print(countPrimes(499979))
