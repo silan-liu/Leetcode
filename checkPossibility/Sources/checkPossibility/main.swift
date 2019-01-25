106734151ba37427588bbdbca876e16790e89841
silan-liu <413769707@qq.com>
2019-01-25 19:28:01 +0800

    add https://leetcode.com/problems/non-decreasing-array/submissions/

--- checkPossibility/Sources/checkPossibility/main.swift
+++ checkPossibility/Sources/checkPossibility/main.swift
@@ -1,1 +1,29 @@
-print("Hello, world!")
+// 3,4,2,3
+func checkPossibility(_ nums: [Int]) -> Bool {
+    
+    if nums.count <= 2 {
+        return true
+    }
+    
+    var i = 0
+    var j = i + 1
+    var count = 0
+    while j < nums.count {
+        if nums[i] <= nums[j] {
+            i += 1
+            j += 1
+        } else {
+            count += 1
+            if count >= 2 {
+                return false
+            }
+
+            j += 1
+        }
+    }
+
+    return true
+}
+
+let nums = [-1,4,2,3]
+print(checkPossibility(nums))
