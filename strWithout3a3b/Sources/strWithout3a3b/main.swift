59fe5ecd9f5e6c634a0cb623a17ff2d9b7717cf4
silan-liu <413769707@qq.com>
2019-01-29 15:04:52 +0800

    add https://leetcode.com/problems/string-without-aaa-or-bbb/submissions/

--- strWithout3a3b/Sources/strWithout3a3b/main.swift
+++ strWithout3a3b/Sources/strWithout3a3b/main.swift
@@ -1,1 +1,61 @@
-print("Hello, world!")
+
+func strWithout3a3b(_ A: Int, _ B: Int) -> String {
+    
+    let lenA = A / 2
+    var realLenA = lenA
+    if A % 2 != 0 {
+        realLenA += 1
+    }
+
+    let lenB = B / 2
+    var realLenB = lenB
+    if B % 2 != 0 {
+        realLenB += 1
+    }
+    
+    var i = 0
+    var result = ""
+    while i < realLenA || i < realLenB {
+        if B / A > 2 {
+            if i < realLenB {
+                if i < lenB {
+                    result += "bb"
+                } else {
+                    result += "b"
+                }
+            } 
+
+            if i < realLenA {
+                if i < lenA {
+                    result += "aa"
+                } else {
+                    result += "a"
+                }
+            }
+        } else {
+            if i < realLenA {
+                if i < lenA {
+                    result += "aa"
+                } else {
+                    result += "a"
+                }
+            } 
+
+            if i < realLenB {
+                if i < lenB {
+                    result += "bb"
+                } else {
+                    result += "b"
+                }
+            }
+        }
+        
+
+        i += 1
+    }
+
+
+    return result
+}
+
+print(strWithout3a3b(3,6))
