209724c6efaa7ae592dd353700fb404a58e2ba3a
silan-liu <413769707@qq.com>
2019-02-18 14:55:01 +0800

    add https://leetcode.com/problems/insert-into-a-binary-search-tree/submissions/

--- insertIntoBST/Sources/insertIntoBST/main.swift
+++ insertIntoBST/Sources/insertIntoBST/main.swift
@@ -1,1 +1,37 @@
-print("Hello, world!")
+// https://leetcode.com/problems/insert-into-a-binary-search-tree/submissions/
+// Definition for a binary tree node.
+ public class TreeNode {
+     public var val: Int
+     public var left: TreeNode?
+     public var right: TreeNode?
+     public init(_ val: Int) {
+         self.val = val
+         self.left = nil
+         self.right = nil
+     }
+ }
+ 
+class Solution {
+    func insertIntoBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        if root == nil {
+            return TreeNode(val)
+        } else  {
+            if let root = root {
+                if root.val < val {
+                   root.right = insertIntoBST(root.right, val)
+                } else {
+                   root.left = insertIntoBST(root.left, val)
+                }
+            }
+
+            return root
+        }
+    }
+}
+
+let s = Solution()
+let root = TreeNode(3)
+let left = TreeNode(2)
+let right = TreeNode(5)
+
+print(s.insertIntoBST(root, 10)?.val)
