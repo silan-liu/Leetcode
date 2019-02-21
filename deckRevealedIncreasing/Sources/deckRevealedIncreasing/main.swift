cb5919e4e3e66d8d2eb3ff17413b41af5db6e92e
silan-liu <413769707@qq.com>
2019-02-21 11:48:52 +0800

    add https://leetcode.com/problems/reveal-cards-in-increasing-order/submissions/

--- deckRevealedIncreasing/Sources/deckRevealedIncreasing/main.swift
+++ deckRevealedIncreasing/Sources/deckRevealedIncreasing/main.swift
@@ -1,5 +1,33 @@
+// https://leetcode.com/problems/reveal-cards-in-increasing-order/submissions/
 class Solution {
     func deckRevealedIncreasing(_ deck: [Int]) -> [Int] {
-        return []
+        let sortedDeck = deck.sorted()
+
+        var result = [Int]()
+        // 按结果倒推
+        var i = sortedDeck.count - 1
+        while i >= 0 {
+            let value = sortedDeck[i]
+            if result.count <= 1 {
+                // 插入最前面
+                result.insert(value, at: 0)
+            } else {
+                // 把最后一个数移到第一个
+                let last = result.last!
+                result.insert(last, at: 0)
+                result.removeLast()
+
+                // 插入最前面
+                result.insert(value, at: 0)
+            }
+
+            i -= 1
+        }
+
+        return result
     }
 }
+
+let s = Solution()
+let deck = [17,5,4,8]
+print(s.deckRevealedIncreasing(deck))
