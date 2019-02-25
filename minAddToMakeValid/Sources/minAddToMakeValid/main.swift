fcbdea1ecf32a02263b1462c901a9bc48dbabd2b
silan-liu <413769707@qq.com>
2019-02-25 09:54:53 +0800

    ad https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/submissions/

--- minAddToMakeValid/Sources/minAddToMakeValid/main.swift
+++ minAddToMakeValid/Sources/minAddToMakeValid/main.swift
@@ -1,1 +1,33 @@
-print("Hello, world!")
+// https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/submissions/
+class Solution {
+    func minAddToMakeValid(_ S: String) -> Int {
+        if S.isEmpty {
+            return 0
+        }
+
+        var stack = [Character]()
+        for s in S {
+            if s == "(" {
+                stack.append(s)
+            } else if s == ")" {
+                if stack.count > 0 {
+                    let top = stack.last!
+                    // 匹配
+                    if top == "(" {
+                        stack.removeLast()
+                    } else {
+                        stack.append(s)
+                    }
+                } else {
+                    stack.append(s)
+                }
+            }
+        }
+
+        return stack.count
+    }
+}
+
+let s = Solution()
+let S = "()))))()))"
+print(s.minAddToMakeValid(S))
