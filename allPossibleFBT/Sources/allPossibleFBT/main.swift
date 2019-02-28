139839432fafa41e809d2e19c98f035d6863b7ca
silan-liu <413769707@qq.com>
2019-02-28 20:09:48 +0800

    add https://leetcode.com/problems/all-possible-full-binary-trees/submissions/

--- allPossibleFBT/Sources/allPossibleFBT/main.swift
+++ allPossibleFBT/Sources/allPossibleFBT/main.swift
@@ -1,4 +1,5 @@
 
+// https://leetcode.com/problems/all-possible-full-binary-trees/submissions/
 // Definition for a binary tree node.
 public class TreeNode {
     public var val: Int
@@ -15,32 +16,42 @@ class Solution {
     var list: [TreeNode?] = [TreeNode]()
 
     func allPossibleFBT(_ N: Int) -> [TreeNode?] {
-        return []
+        // 必须为奇数才满足。
+        if N < 0 || N % 2 == 0 {
+            return []
+        }
+
+        return recursive(N)
     }
 
-    func buildTree(_ N: inout Int, _ curNode: TreeNode?, _ root: inout TreeNode?) {
-        if N == 0 {
-            list.append(root)
+    func recursive(_ N: Int) -> [TreeNode?] {
+        var list = [TreeNode?]()
+        if N == 1 {
+            let node = TreeNode(0)
+            list.append(node)
         } else {
-            if root == nil {
-                // 生成root
-                root = TreeNode(0)
-                N -= 1
+            var i = 1
+            // 比如N = 7，那么左右节点个数组合为(1,5),(3,3),(5,1)，然后再对子树的进行细分。
+            while i < N {
+                let leftList = allPossibleFBT(i)
+                let rightList = allPossibleFBT(N - i - 1)
 
-                if N >= 2 {
-                    // 生成左右子树
-                    root?.left = TreeNode(0)
-                    root?.right = TreeNode(0)
+                for leftNode in leftList {
+                    for rightNode in rightList {
+                        // 节点组合
+                        let node = TreeNode(0)
+                        node.left = leftNode
+                        node.right = rightNode
 
-                    N -= 2
-
-                    // 左边有2个节点
-                    buildTree(&N, root?.left, &root) 
-
-                    // 右边有2个节点
-                    buildTree(&N, root?.right, &root)
+                        // 最终的根节点
+                        list.append(node)
+                    }
                 }
+
+                i += 2
             }
         }
-    } 
+
+        return list
+    }
 }
