f5f24ee4801c766f44ca4a3c6a4f1ab59bfb4a32
silan-liu <413769707@qq.com>
2019-03-05 20:57:42 +0800

    add template

--- distributeCoins/Sources/distributeCoins/main.swift
+++ distributeCoins/Sources/distributeCoins/main.swift
@@ -1,1 +1,17 @@
-print("Hello, world!")
+// https://leetcode.com/problems/distribute-coins-in-binary-tree/
+public class TreeNode {
+     public var val: Int
+     public var left: TreeNode?
+     public var right: TreeNode?
+     public init(_ val: Int) {
+         self.val = val
+         self.left = nil
+         self.right = nil
+     }
+}
+ 
+class Solution {
+    func distributeCoins(_ root: TreeNode?) -> Int {
+        return 1
+    }
+}
