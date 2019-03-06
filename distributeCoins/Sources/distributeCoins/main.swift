ef08d16c36d978d6a03b281428dc2e6054cf69c5
silan-liu <413769707@qq.com>
2019-03-06 11:53:10 +0800

    add https://leetcode.com/problems/distribute-coins-in-binary-tree/

--- distributeCoins/Sources/distributeCoins/main.swift
+++ distributeCoins/Sources/distributeCoins/main.swift
@@ -11,7 +11,24 @@ public class TreeNode {
 }
  
 class Solution {
+    var move = 0
     func distributeCoins(_ root: TreeNode?) -> Int {
-        return 1
+        _ = dfs(root)
+        return move
+    }
+
+    // 计算子节点给父节点的个数
+    func dfs(_ root: TreeNode?) -> Int {
+        if let root = root {
+            let left = dfs(root.left)
+            let right = dfs(root.right)
+
+            move += abs(left) + abs(right)
+
+            // 返回移动步数，自己要留1，所以root.val - 1。
+            return root.val - 1 + left + right
+        }
+
+        return 0
     }
 }
