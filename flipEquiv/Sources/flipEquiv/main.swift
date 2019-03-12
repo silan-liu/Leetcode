b9d1ffff2472d0e99886e8357498f8eca7773106
silan-liu <413769707@qq.com>
2019-03-12 20:05:47 +0800

    faster solution

--- flipEquiv/Sources/flipEquiv/main.swift
+++ flipEquiv/Sources/flipEquiv/main.swift
@@ -26,4 +26,30 @@ class Solution {
 
         return false
     }
+
+    // more faster
+    func flipEquiv2(_ root1: TreeNode?, _ root2: TreeNode?) -> Bool {
+        
+        if let root1 = root1, let root2 = root2, root1.val == root2.val {
+            if isEqual(root1.left, root2.left), isEqual(root1.right, root2.right) {
+                return flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right)
+            } else if isEqual(root1.left, root2.right), isEqual(root1.right, root2.left) {
+                return flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left)
+            }
+        } else if root1 == nil, root2 == nil {
+            return true
+        }
+
+        return false
+    }
+
+    func isEqual(_ root1: TreeNode?, _ root2: TreeNode?) -> Bool {
+        if let root1 = root1, let root2 = root2, root1.val == root2.val {
+            return true
+        } else if root1 == nil, root2 == nil {
+            return true
+        }
+
+        return false
+    }
 }
