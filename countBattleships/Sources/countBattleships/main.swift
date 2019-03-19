e12e326f7b67968d7512fc7ac208455c7892159a
silan-liu <413769707@qq.com>
2019-03-19 10:58:41 +0800

    add https://leetcode.com/problems/battleships-in-a-board/submissions/

--- countBattleships/Sources/countBattleships/main.swift
+++ countBattleships/Sources/countBattleships/main.swift
@@ -1,1 +1,43 @@
-print("Hello, world!")
+// https://leetcode.com/problems/battleships-in-a-board
+func countBattleships(_ board: [[Character]]) -> Int {
+    let rows = board.count
+    var columns = 0
+    if rows > 0 {
+        columns = board[0].count
+    }
+
+    var count = 0
+    var i = 0
+
+    while i < rows {
+        
+        for j in 0..<columns {
+            if board[i][j] == "." {
+                continue
+            }
+
+            // left
+            if j > 0, board[i][j - 1] == "X" {
+                continue
+            }
+
+            // top
+            if i > 0, board[i - 1][j] == "X" {
+                continue
+            }
+
+            count += 1
+        }
+
+        i += 1
+    }
+
+    return count
+}
+
+let board = [[Character("X"), Character("X"), Character("."), Character("X")],
+            [Character("."), Character("."), Character("."), Character("X")],
+            [Character("."), Character("."), Character("."), Character("X")]]
+
+print(countBattleships(board))
+
