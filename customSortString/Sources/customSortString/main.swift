f7afd118e617f0cd7548a43877efc9f497138fc2
silan-liu <413769707@qq.com>
2019-04-01 17:20:04 +0800

    add https://leetcode.com/problems/custom-sort-string/submissions/

--- customSortString/Sources/customSortString/main.swift
+++ customSortString/Sources/customSortString/main.swift
@@ -1,8 +1,53 @@
+// https://leetcode.com/problems/custom-sort-string/submissions/
 class Solution {
     func customSortString(_ S: String, _ T: String) -> String {
         // 记录字符的index
-        var map = [Character: Int]
+        var map = [Character: Int]()
 
-        return ""
+        var i = 0
+        while i < S.count {
+            
+            let index = S.index(S.startIndex, offsetBy: i)
+            let char = S[index]
+            
+            map[char] = i
+            
+            i += 1
+        }
+        
+        // 存在的字符
+        var existCharList = [Character]()
+        
+        // 剩余的字符
+        var leftCharList = [Character]()
+        
+        i = 0
+        while i < T.count {
+            
+            let index = T.index(S.startIndex, offsetBy: i)
+            let char = T[index]
+            
+            if let order = map[char] {
+                // 找到合适的位置
+                var j = existCharList.count - 1
+                while j >= 0, map[existCharList[j]]! >= order {
+                    j -= 1
+                }
+                
+                existCharList.insert(char, at: j + 1)
+                
+            } else {
+                leftCharList.append(char)
+            }
+            
+            i += 1
+        }
+        
+        let list = existCharList + leftCharList
+        return String(list)
     }
 }
+
+let S = "dcba", T = "abcddc"
+let s = Solution()
+print(s.customSortString(S, T))
