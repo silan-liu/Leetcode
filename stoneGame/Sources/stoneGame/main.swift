d0d5a3cfecc3391a0bc202fbc67df90f05c422f9
silan-liu <413769707@qq.com>
2019-04-02 14:19:14 +0800

    add https://leetcode.com/problems/stone-game/solution/

--- stoneGame/Sources/stoneGame/main.swift
+++ stoneGame/Sources/stoneGame/main.swift
@@ -1,1 +1,10 @@
-print("Hello, world!")
+class Solution {
+    func stoneGame(_ piles: [Int]) -> Bool {
+        return true
+    }
+}
+
+
+let s = Solution()
+let piles = [2,2,1,3,5,2]
+print(s.stoneGame(piles))
