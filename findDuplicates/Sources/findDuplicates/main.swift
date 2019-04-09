67f6b52007a27f89cea2931145c06377217c1024
silan-liu <413769707@qq.com>
2019-04-09 17:50:44 +0800

    add https://leetcode.com/problems/find-all-duplicates-in-an-array/submissions/

--- findDuplicates/Sources/findDuplicates/main.swift
+++ findDuplicates/Sources/findDuplicates/main.swift
@@ -1,1 +1,28 @@
-print("Hello, world!")
+// https://leetcode.com/problems/find-all-duplicates-in-an-array/submissions/
+class Solution {
+    func findDuplicates(_ nums: [Int]) -> [Int] {
+        var list = nums
+        var result = [Int]()
+        
+        // 逐个遍历，将 num 所对应的 index 设置为负数。
+        for num in list {
+            let positiveNum = abs(num)
+            if positiveNum >= 1 && positiveNum <= list.count {
+                let n = list[positiveNum - 1]
+                
+                // 已经置为负数，说明有相同的数
+                if n < 0 {
+                    result.append(positiveNum)
+                } else {
+                    list[positiveNum - 1] = -n
+                }
+            }
+        }
+        
+        return result
+    }
+}
+
+let s = Solution()
+let nums = [2,2]
+print(s.findDuplicates(nums))
