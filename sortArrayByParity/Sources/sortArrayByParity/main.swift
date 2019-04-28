8a9fa3f8e35d748e0833bae0d4de84149fa1f316
silan-liu <413769707@qq.com>
2019-04-28 21:18:11 +0800

    add https://leetcode.com/problems/sort-array-by-parity/submissions/

--- sortArrayByParity/Sources/sortArrayByParity/main.swift
+++ sortArrayByParity/Sources/sortArrayByParity/main.swift
@@ -1,1 +1,23 @@
-print("Hello, world!")
+// https://leetcode.com/problems/sort-array-by-parity/submissions/
+class Solution {
+    func sortArrayByParity(_ A: [Int]) -> [Int] {
+        // 偶数在前，奇数在后
+        var evenList = [Int]()
+        var oddList = [Int]()
+        
+        for n in A {
+            if n % 2 == 0 {
+                evenList.append(n)
+            } else {
+                oddList.append(n)
+            }
+        }
+        
+        return evenList + oddList
+    }
+}
+
+let s = Solution()
+let A = [0,3,1,2,4]
+print(s.sortArrayByParity(A))
+
