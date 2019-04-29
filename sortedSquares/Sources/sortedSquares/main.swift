ad7fc71fdb80cdca6aa8afb7d5c2d15f9fb7051e
silan-liu <413769707@qq.com>
2019-04-29 10:09:09 +0800

    add https://leetcode.com/problems/squares-of-a-sorted-array/submissions/

--- sortedSquares/Sources/sortedSquares/main.swift
+++ sortedSquares/Sources/sortedSquares/main.swift
@@ -1,1 +1,15 @@
-print("Hello, world!")
+// https://leetcode.com/problems/squares-of-a-sorted-array/submissions/
+class Solution {
+    func sortedSquares(_ A: [Int]) -> [Int] {
+        var squares = [Int]()
+        for n in A {
+            squares.append(n * n)
+        }
+        
+        return squares.sorted()
+    }
+}
+
+let s = Solution()
+let A = [-7,-3,2,3,11]
+print(s.sortedSquares(A))
