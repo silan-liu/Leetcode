be9b2f5dc4a7c5211ed8091347f37ce2fa59b3a0
silan-liu <413769707@qq.com>
2019-04-30 18:45:53 +0800

    add solution

--- diStringMatch/Sources/diStringMatch/main.swift
+++ diStringMatch/Sources/diStringMatch/main.swift
@@ -1,1 +1,35 @@
-print("Hello, world!")
+
+// https://leetcode.com/problems/di-string-match/
+class Solution {
+    // 60%
+    func diStringMatch(_ S: String) -> [Int] {
+        var countOfI = 0
+        var countOfD = 0
+        
+        var result = [Int]()
+        let n = S.count
+        
+        for s in S {
+            // <
+            if s == "I" {
+                countOfI += 1
+                
+                result.append(countOfI - 1)
+                
+            } else if s == "D" {
+                // >
+                countOfD += 1
+                
+                result.append(n + 1 - countOfD)
+            }
+        }
+        
+        result.append(countOfI)
+        
+        return result
+    }
+}
+
+let s = Solution()
+let S = "DDI"
+print(s.diStringMatch(S))
