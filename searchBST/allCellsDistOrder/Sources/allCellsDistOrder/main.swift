8dd3a191c7800f8292c8fcce61ced46301adeae6
silan-liu <413769707@qq.com>
2019-05-06 19:07:15 +0800

    添加模板。

--- searchBST/allCellsDistOrder/Sources/allCellsDistOrder/main.swift
+++ searchBST/allCellsDistOrder/Sources/allCellsDistOrder/main.swift
@@ -1,1 +1,8 @@
-print("Hello, world!")
+
+class Solution {
+    func allCellsDistOrder(_ R: Int, _ C: Int, _ r0: Int, _ c0: Int) -> [[Int]] {
+        var result = [[Int]]()
+        
+        return result
+    }
+}
