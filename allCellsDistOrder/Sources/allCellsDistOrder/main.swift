de64394cf19d33bb206d78eebe0c03b148fc9fa9
silan-liu <413769707@qq.com>
2019-05-07 15:40:19 +0800

    add .

--- allCellsDistOrder/Sources/allCellsDistOrder/main.swift
+++ allCellsDistOrder/Sources/allCellsDistOrder/main.swift
@@ -1,14 +1,48 @@
 
+// https://leetcode.com/problems/matrix-cells-in-distance-order/
 class Solution {
     func allCellsDistOrder(_ R: Int, _ C: Int, _ r0: Int, _ c0: Int) -> [[Int]] {
-        var result = [[Int]]()
+        var lenList = [[[Int]]]()
+        
         // 最大值 R + C
         var i = 0
         while i <= R + C + 1 {
-            result.append([])
+            lenList.append([])
+            i += 1
+        }
+        
+        var j = 0
+        while j < R {
+            var k = 0
+            while k < C {
+                let len = abs(j - r0) + abs(k - c0)
+                if len >= 0 && len < lenList.count {
+                    lenList[len].append([j, k])
+                }
+                
+                k += 1
+            }
+            
+            j += 1
+        }
+        
+        // 去除空数组
+       var result = [[Int]]()
+        i = 0
+        while i < lenList.count {
+            if lenList[i].count > 0 {
+                result.append(contentsOf: lenList[i])
+            }
+            
             i += 1
         }
         
         return result
     }
 }
+
+let s = Solution()
+
+let R = 2, C = 3, r0 = 1, c0 = 2
+
+print(s.allCellsDistOrder(R, C, r0, c0))
