9d7d00d79fbac7681deea53954e8b12cf83ea78d
silan-liu <413769707@qq.com>
2019-05-07 10:33:03 +0800

    add

--- allCellsDistOrder/Sources/allCellsDistOrder/main.swift
+++ allCellsDistOrder/Sources/allCellsDistOrder/main.swift
@@ -2,6 +2,12 @@
 class Solution {
     func allCellsDistOrder(_ R: Int, _ C: Int, _ r0: Int, _ c0: Int) -> [[Int]] {
         var result = [[Int]]()
+        // 最大值 R + C
+        var i = 0
+        while i <= R + C + 1 {
+            result.append([])
+            i += 1
+        }
         
         return result
     }
