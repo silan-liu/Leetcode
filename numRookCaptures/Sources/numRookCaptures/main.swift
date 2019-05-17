d694dca505f06a99a89c946ad92088d4d9d242ee
silan-liu <413769707@qq.com>
2019-05-17 18:47:41 +0800

    add https://leetcode.com/problems/available-captures-for-rook/submissions/

--- numRookCaptures/Sources/numRookCaptures/main.swift
+++ numRookCaptures/Sources/numRookCaptures/main.swift
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/available-captures-for-rook/submissions/
 class Solution {
     var flagList = [[Bool]]()
     var row = 0
@@ -19,7 +20,6 @@ class Solution {
                 // init flagList
                 var i = 0
                 while i < row {
-                    var list = [Bool]()
                     
                     var j = 0
                     while j < column {
@@ -28,113 +28,43 @@ class Solution {
                             startX = i
                             startY = j
                             
-                            list.append(true)
-                        } else {
-                            list.append(false)
+                            return capture(board, 0, 1, startX, startY) + capture(board, 0, -1, startX, startY) + capture(board, -1, 0, startX, startY) + capture(board, 1, 0, startX, startY)
+                            
                         }
                         
                         j += 1
                     }
                     
-                    flagList.append(list)
-                    
                     i += 1
                 }
-                
-                recursive(startX, startY, board, board[startX][startY])
             }
         }
         
         return count
     }
     
-    func recursive(_ x: Int, _ y: Int, _ board: [[Character]], _ last: Character) {
-        if x >= 0, x < row, y >= 0, y < column {
-            // 上
-            if x - 1 >= 0 {
-                // 未走过，并且非同色
-                let char = board[x - 1][y]
-                
-                if !flagList[x - 1][y] && !isSameColor(char, last) {
-                    // 未走过
-                    if char == "p" {
-                        print("x:\(x-1), y:\(y)")
-                        count += 1
-                    }
-                    
-                    flagList[x - 1][y] = true
-                    recursive(x - 1, y, board, last)
-                }
+    // 一直沿着一个方向走，遇到 B/p 停止
+    func capture(_ board: [[Character]], _ dx: Int, _ dy: Int, _ x: Int, _ y: Int) -> Int {
+        var m_x = x
+        var m_y = y
+        
+        while m_x >= 0, m_x < row, m_y >= 0, m_y < column {
+            
+            let char = board[m_x][m_y]
+            if char == "B" {
+                return 0
             }
             
-            // 下
-            if x + 1 < row {
-                // 未走过，并且非同色
-                let char = board[x + 1][y]
-                
-                if !flagList[x + 1][y] && !isSameColor(char, last) {
-                    // 未走过
-                    if char == "p" {
-                        print("x:\(x+1), y:\(y)")
-                        count += 1
-                    }
-                    
-                    flagList[x + 1][y] = true
-                    recursive(x + 1, y, board, last)
-                }
+            if char == "p" {
+                return 1
             }
             
-            // 左
-            if y - 1 >= 0 {
-                // 未走过，并且非同色
-                let char = board[x][y - 1]
-                
-                if !flagList[x][y - 1] && !isSameColor(char, last) {
-                    // 未走过
-                    if char == "p" {
-                        print("x:\(x), y:\(y - 1)")
-                        count += 1
-                    }
-                    
-                    flagList[x][y - 1] = true
-
-                    recursive(x, y - 1, board, char)
-                }
-            }
-            
-            // 右
-            if y + 1 < column {
-                // 未走过，并且非同色
-                let char = board[x][y + 1]
-                
-                if !flagList[x][y + 1] && !isSameColor(char, last) {
-                    // 未走过
-                    if char == "p" {
-                        print("x:\(x), y:\(y + 1)")
-
-                        count += 1
-                    }
-                    
-                    flagList[x][y + 1] = true
-
-                    recursive(x, y + 1, board, char)
-                }
-            }
-        }
-    }
-    
-    func isSameColor(_ x: Character, _ y: Character) -> Bool {
-
-        // 同为大小写
-        if x >= "a", x <= "z", y >= "a", y <= "z" {
-            return true
+            m_x += dx
+            m_y += dy
         }
         
-        if x >= "A", x <= "Z", y >= "A", y <= "Z" {
-            return true
-        }
         
-        return false
+        return 0
     }
 }
 
@@ -144,5 +74,3 @@ let s = Solution()
 let board = [[Character("."),Character("."),Character("."),Character("."),Character("."),Character("."),Character("."),Character(".")],[Character("."),Character("."),Character("."),Character("p"),Character("."),Character("."),Character("."),Character(".")],[Character("."),Character("."),Character("."),Character("p"),Character("."),Character("."),Character("."),Character(".")],[Character("p"),Character("p"),Character("."),Character("R"),Character("."),Character("p"),Character("B"),Character(".")],[Character("."),Character("."),Character("."),Character("."),Character("."),Character("."),Character("."),Character(".")],[Character("."),Character("."),Character("."),Character("B"),Character("."),Character("."),Character("."),Character(".")],[Character("."),Character("."),Character("."),Character("p"),Character("."),Character("."),Character("."),Character(".")],[Character("."),Character("."),Character("."),Character("."),Character("."),Character("."),Character("."),Character(".")]]
 
 print(s.numRookCaptures(board))
-
-print(s.isSameColor(Character("R"), Character("B")))
