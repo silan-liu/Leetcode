cefe9f24fd43ee3e38140df143a40ccd5b9c4f12
silan-liu <413769707@qq.com>
2019-05-18 21:30:34 +0800

    init

---
+++ isBoomerang/README.md
@@ -0,0 +1,3 @@
+# isBoomerang
+
+A description of this package.
---
+++ isBoomerang/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import isBoomerangTests
+
+var tests = [XCTestCaseEntry]()
+tests += isBoomerangTests.allTests()
+XCTMain(tests)
