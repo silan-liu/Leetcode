7529c5010ccebc50f465ba0a6690d9121fe6cd76
silan-liu <413769707@qq.com>
2019-05-20 11:24:58 +0800

    add recursion-cache

--- fib/Sources/fib/main.swift
+++ fib/Sources/fib/main.swift
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/fibonacci-number/submissions/
 class Solution {
+    var map = [Int: Int]()
+
     // 递归 13.03%
     func fib(_ N: Int) -> Int {
         if N == 0 {
@@ -8,6 +10,30 @@ class Solution {
             return 1
         } else{
             return fib(N - 1) + fib(N - 2)
+        }
+    }
+    
+    // 递归，100.00%
+    func fib3(_ N: Int) -> Int {
+
+        return fib_cache(N)
+    }
+    
+    // 对递归进行优化，缓存已计算的值
+    func fib_cache(_ N: Int) -> Int {
+        
+        if N == 0 {
+            return 0
+        } else if N == 1 {
+            return 1
+        } else{
+            if let value = map[N] {
+                return value
+            }
+            
+            let reuslt = fib_cache(N - 1) + fib_cache(N - 2)
+            map[N] = reuslt
+            return reuslt
         }
     }
     
@@ -38,4 +64,5 @@ class Solution {
 
 let s = Solution()
 print(s.fib(10))
+print(s.fib3(20))
 print(s.fib2(3))
