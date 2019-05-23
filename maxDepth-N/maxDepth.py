de8362b9186cfd8c93e32044b0419ab31b1c453a
silan-liu <413769707@qq.com>
2019-05-23 11:58:17 +0800

    add 27.06%

---
+++ maxDepth-N/maxDepth.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+
+# https://leetcode.com/problems/maximum-depth-of-n-ary-tree/submissions/
+class Solution(object):
+    # 27.06%
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        q1 = []
+        level = 0
+
+        if root != None:
+            q1.append(root)
+
+        while len(q1) > 0:
+            level += 1
+
+            q2 = []
+
+            for val in q1:
+                for child in val.children:
+                    q2.append(child)
+
+            q1 = q2
+
+        return level
