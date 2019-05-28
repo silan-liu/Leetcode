022bf418db4079d51138cf3641729f35ad4b81b2
silan-liu <413769707@qq.com>
2019-05-28 10:21:11 +0800

    add https://leetcode.com/problems/height-checker/submissions/

--- heightChecker/Sources/heightChecker/main.swift
+++ heightChecker/Sources/heightChecker/main.swift
@@ -1,1 +1,22 @@
-print("Hello, world!")
+// https://leetcode.com/problems/height-checker/submissions/
+class Solution {
+    func heightChecker(_ heights: [Int]) -> Int {
+        let sortedHeights = heights.sorted()
+        var i = 0
+        var count = 0
+        while i < heights.count {
+            if sortedHeights[i] != heights[i] {
+                count += 1
+            }
+            
+            i += 1
+        }
+        
+        return count
+    }
+}
+
+
+let height = [1,1,4,2,1,3]
+let s = Solution()
+print(s.heightChecker(height))
