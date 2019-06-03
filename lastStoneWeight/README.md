2dc695d63075b2aa467a5c6218f16411d72d2c42
silan-liu <413769707@qq.com>
2019-06-03 13:54:22 +0800

    init

---
+++ lastStoneWeight/README.md
@@ -0,0 +1,3 @@
+# lastStoneWeight
+
+A description of this package.
---
+++ lastStoneWeight/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import lastStoneWeightTests
+
+var tests = [XCTestCaseEntry]()
+tests += lastStoneWeightTests.allTests()
+XCTMain(tests)
