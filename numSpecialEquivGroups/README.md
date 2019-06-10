80ae7510eb8e781b58f9b611463549c07e436d86
silan-liu <413769707@qq.com>
2019-06-10 10:23:22 +0800

    init

---
+++ numSpecialEquivGroups/README.md
@@ -0,0 +1,3 @@
+# numSpecialEquivGroups
+
+A description of this package.
---
+++ numSpecialEquivGroups/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import numSpecialEquivGroupsTests
+
+var tests = [XCTestCaseEntry]()
+tests += numSpecialEquivGroupsTests.allTests()
+XCTMain(tests)
