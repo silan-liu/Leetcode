f1f66ff2e6f843d036c0bfacfd149a02068315f5
silan-liu <413769707@qq.com>
2019-06-11 21:41:05 +0800

    init

---
+++ levelOrder/levelOrder.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/n-ary-tree-level-order-traversal/
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, children):
+        self.val = val
+        self.children = children
+"""
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: Node
+        :rtype: List[List[int]]
+        """
+        
