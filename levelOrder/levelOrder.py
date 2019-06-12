ed1a54b1d7d7086c72a1d3c1c3aa8a1606907540
silan-liu <413769707@qq.com>
2019-06-12 12:38:20 +0800

    https://leetcode.com/problems/n-ary-tree-level-order-traversal/

--- levelOrder/levelOrder.py
+++ levelOrder/levelOrder.py
@@ -7,9 +7,38 @@ class Node(object):
         self.children = children
 """
 class Solution(object):
+    #  70.67%
     def levelOrder(self, root):
         """
         :type root: Node
         :rtype: List[List[int]]
         """
+        items = []
+        result = []
+        if root:
+            items.append(root)
+            result.append([root.val])
+
+        i = 0
+        count = len(items)
+        levelList = []
+        while i < count:
+            node = items[i]
+
+            for child in node.children:
+                levelList.append(child.val)
+                items.append(child)
+
+            i += 1
+
+            # 更新，一层遍历完成
+            if i == count:
+                if len(levelList) > 0:
+                    result.append(levelList)
+                count = len(items)
+                levelList = []
+
+        return result
+
+
         
