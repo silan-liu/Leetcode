a0957d39c9c07922654103ded7cffb12efd9bf3a
silan-liu <413769707@qq.com>
2019-06-13 10:27:48 +0800

    add 8.47%

--- bitwiseComplement/Sources/bitwiseComplement/main.swift
+++ bitwiseComplement/Sources/bitwiseComplement/main.swift
@@ -1,1 +1,25 @@
-print("Hello, world!")
+// https://leetcode.com/problems/complement-of-base-10-integer/submissions/
+class Solution {
+    // 8.47%
+    func bitwiseComplement(_ N: Int) -> Int {
+        // 对于每位取反
+        var result = 0
+        var count = 0
+        var tmp = N
+        while tmp != 0 {
+            let bit = tmp & 1
+            let complementBit = 1 - bit
+            result |= (complementBit << count)
+            
+            tmp = tmp >> 1
+            
+            count += 1
+        }
+        
+        return result
+    }
+}
+
+let s = Solution()
+let N = 10
+print(s.bitwiseComplement(N))
