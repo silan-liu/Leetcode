6ef6876e134415c1490e1421af720535176831b2
silan-liu <413769707@qq.com>
2019-06-17 10:30:35 +0800

    add https://leetcode.com/problems/duplicate-zeros/submissions/

--- duplicateZeros/Sources/duplicateZeros/main.swift
+++ duplicateZeros/Sources/duplicateZeros/main.swift
@@ -1,1 +1,28 @@
-print("Hello, world!")
+// https://leetcode.com/problems/duplicate-zeros/submissions/
+class Solution {
+    // 100%
+    func duplicateZeros(_ arr: inout [Int]) {
+        var i = arr.count - 1
+        let count = arr.count
+        while i >= 0 {
+            if arr[i] == 0 {
+                arr.insert(0, at: i)
+            }
+            
+            i -= 1
+        }
+        
+        // 删除超出原本 array 的部分。
+        if arr.count > count {
+            let range = Range.init(uncheckedBounds: (count, arr.count))
+            
+            arr.removeSubrange(range)
+        }
+       
+        print(arr)
+    }
+}
+
+let s = Solution()
+var arr = [1,0,2,3,0,4,5,0]
+s.duplicateZeros(&arr)
