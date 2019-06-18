8cb1699223454a8ee023c64ec036d5093cf1a918
silan-liu <413769707@qq.com>
2019-06-18 10:20:12 +0800

    add // https://leetcode.com/problems/largest-perimeter-triangle/submissions/

--- largestPerimeter/Sources/largestPerimeter/main.swift
+++ largestPerimeter/Sources/largestPerimeter/main.swift
@@ -1,1 +1,33 @@
-print("Hello, world!")
+// https://leetcode.com/problems/largest-perimeter-triangle/submissions/
+class Solution {
+    // 11.11%
+    func largestPerimeter(_ A: [Int]) -> Int {
+        let sortedA = A.sorted { (a, b) -> Bool in
+            return a > b
+        }
+        
+        var i = 0
+        while i <= sortedA.count - 3 {
+            
+            if isTriangle(sortedA[i], sortedA[i+1], sortedA[i+2]) {
+                return sortedA[i] + sortedA[i+1] + sortedA[i+2]
+            }
+            
+            i += 1
+        }
+        
+        return 0
+    }
+    
+    func isTriangle(_ a: Int, _ b: Int, _ c: Int) -> Bool {
+        if a + b > c, a + c > b, c + b > a {
+            return true
+        }
+        
+        return false
+    }
+}
+
+let s = Solution()
+let A = [3,6,2,3]
+print(s.largestPerimeter(A))
