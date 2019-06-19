0415bc997acc4dba579daeb263ad4c16f5668fe3
silan-liu <413769707@qq.com>
2019-06-19 21:20:27 +0800

    simplify solution

--- largestPerimeter/Sources/largestPerimeter/main.swift
+++ largestPerimeter/Sources/largestPerimeter/main.swift
@@ -26,8 +26,28 @@ class Solution {
         
         return false
     }
+    
+    //24.44%
+    func largestPerimeter2(_ A: [Int]) -> Int {
+        let sortedA = A.sorted { (a, b) -> Bool in
+            return a > b
+        }
+        
+        var i = 0
+        while i <= sortedA.count - 3 {
+            
+            if sortedA[i+1] + sortedA[i+2] > sortedA[i] {
+                return sortedA[i] + sortedA[i+1] + sortedA[i+2]
+            }
+            
+            i += 1
+        }
+        
+        return 0
+    }
 }
 
 let s = Solution()
 let A = [3,6,2,3]
 print(s.largestPerimeter(A))
+print(s.largestPerimeter2(A))
