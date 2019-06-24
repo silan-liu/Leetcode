b09f4c4eee4602f67d850a7f4a5be5eaaa159146
silan-liu <413769707@qq.com>
2019-06-24 22:54:03 +0800

    add solution

--- isLongPressedName/Sources/isLongPressedName/main.swift
+++ isLongPressedName/Sources/isLongPressedName/main.swift
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/long-pressed-name/
 class Solution {
+    // 95.00%
     func isLongPressedName(_ name: String, _ typed: String) -> Bool {
         var i = 0
         var j = 0
@@ -7,23 +8,37 @@ class Solution {
         let nameList = Array(name)
         let typedList = Array(typed)
         
+        // 分别遍历计算出 name, typed 相同的字符，进行比较，如果相同且typed中的字符数>name的，则继续。
+        // alex, aaleex
         while i < nameList.count {
-            while j < typedList.count, typedList[j] == nameList[i] {
-                j += 1
-            }
-            
-            if j < typedList.count {
-                i += 1
-                if typedList[j] != nameList[i] {
-                    return false
+
+            if j < typedList.count, nameList[i] == typedList[j] {
+                var c1 = 1
+                
+                while i + 1 < nameList.count, nameList[i] == nameList[i+1] {
+                    i += 1
+                    c1 += 1
                 }
-            } else {
-                if i != nameList.count - 1 {
+                
+                var c2 = 1
+                while j + 1 < typedList.count, typedList[j] == typedList[j+1] {
+                    j += 1
+                    c2 += 1
+                }
+                
+                if c2 < c1 {
                     return false
                 } else {
                     i += 1
+                    j += 1
                 }
+            } else {
+                return false
             }
+        }
+        
+        if j < typedList.count {
+            return false
         }
         
         return true
@@ -31,7 +46,7 @@ class Solution {
 }
 
 let s = Solution()
-let name = "aab", typed = "aabb"
+let name = "abbb", typed = "abb"
 
 print(s.isLongPressedName(name, typed))
 
