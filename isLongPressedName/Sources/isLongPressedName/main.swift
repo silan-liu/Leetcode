f5c9eb80ca1a529c792dfda39704eccd37bf5e3d
silan-liu <413769707@qq.com>
2019-06-24 23:34:54 +0800

    add another solution

--- isLongPressedName/Sources/isLongPressedName/main.swift
+++ isLongPressedName/Sources/isLongPressedName/main.swift
@@ -48,10 +48,35 @@ class Solution {
         
         return true
     }
+    
+    // 双指针
+    func isLongPressedName2(_ name: String, _ typed: String) -> Bool {
+        let nameList = Array(name)
+        let typedList = Array(typed)
+        
+        let m = nameList.count
+        var i = 0
+        var j = 0
+        
+        while j < typedList.count {
+            // 相等，继续遍历，不相等时，判断 typepList 与前一个字符是否相等
+            if i < m, nameList[i] == typedList[j] {
+                i += 1
+            } else if j == 0 || typedList[j] != typedList[j - 1] {
+                return false
+            }
+            
+            j += 1
+        }
+        
+        return i == m
+    }
 }
 
 let s = Solution()
-let name = "abbb", typed = "abb"
+let name = "laiden", typed = "laiden"
 
 print(s.isLongPressedName(name, typed))
+print(s.isLongPressedName2(name, typed))
+
 
