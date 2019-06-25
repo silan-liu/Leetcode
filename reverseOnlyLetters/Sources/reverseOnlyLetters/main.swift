1c9727b91dc47bf0b0ffdbf52a5377ded0dd66e4
silan-liu <413769707@qq.com>
2019-06-25 20:07:39 +0800

    add https://leetcode.com/problems/reverse-only-letters/submissions/

--- reverseOnlyLetters/Sources/reverseOnlyLetters/main.swift
+++ reverseOnlyLetters/Sources/reverseOnlyLetters/main.swift
@@ -1,1 +1,37 @@
-print("Hello, world!")
+// https://leetcode.com/problems/reverse-only-letters/submissions/
+class Solution {
+    // 90.20%
+    func reverseOnlyLetters(_ S: String) -> String {
+        var list = Array(S)
+        var i = 0
+        var j = list.count - 1
+        
+        while i <= j {
+            while i < list.count, !list[i].isLetter {
+                i += 1
+            }
+            
+            while j >= 0, !list[j].isLetter {
+                j -= 1
+            }
+            
+            if i < j, i < list.count, j >= 0 {
+                // swap
+                let tmp = list[i]
+                list[i] = list[j]
+                list[j] = tmp
+                
+                i += 1
+                j -= 1
+            } else {
+                break
+            }
+        }
+        
+        return String(list)
+    }
+}
+
+let s = Solution()
+let S = "ab-cd"
+print(s.reverseOnlyLetters(S))
