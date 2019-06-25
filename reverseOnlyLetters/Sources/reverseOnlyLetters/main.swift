6e66f30bf709f9a16550856f3fc42a25de741b46
silan-liu <413769707@qq.com>
2019-06-25 20:26:37 +0800

    add solution

--- reverseOnlyLetters/Sources/reverseOnlyLetters/main.swift
+++ reverseOnlyLetters/Sources/reverseOnlyLetters/main.swift
@@ -55,9 +55,42 @@ class Solution {
         
         return result
     }
+    
+    // 45.10%
+    func reverseOnlyLetters3(_ S: String) -> String {
+        var stack = [Character]()
+        var list = Array(S)
+
+        var i = 0
+
+        while i < list.count {
+            if list[i].isLetter {
+                stack.append(list[i])
+            }
+            
+            i += 1
+        }
+        
+        var result = ""
+        i = 0
+        while i < list.count {
+            if list[i].isLetter {
+                result += String(stack.last!)
+                stack.removeLast()
+            } else {
+                result += String(list[i])
+            }
+            
+            i += 1
+        }
+        
+        return result
+    }
 }
 
 let s = Solution()
 let S = "dd-aco0fs!f-0kk"
+
 print(s.reverseOnlyLetters(S))
 print(s.reverseOnlyLetters2(S))
+print(s.reverseOnlyLetters3(S))
