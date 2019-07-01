b801c4990f8d9b09d2a3faf0bfd66ee4046f29d7
silan-liu <413769707@qq.com>
2019-07-01 11:44:03 +0800

    add https://leetcode.com/problems/partition-array-into-three-parts-with-equal-sum

--- canThreePartsEqualSum/Sources/canThreePartsEqualSum/main.swift
+++ canThreePartsEqualSum/Sources/canThreePartsEqualSum/main.swift
@@ -1,1 +1,75 @@
-print("Hello, world!")
+// https://leetcode.com/problems/partition-array-into-three-parts-with-equal-sum
+class Solution {
+    // 34.88%
+    func canThreePartsEqualSum(_ A: [Int]) -> Bool {
+        var sum = 0
+        for num in A {
+            sum += num
+        }
+        
+        if sum % 3 != 0 {
+            return false
+        }
+        
+        var i = 0
+        var j = A.count - 1
+        
+        let avg = sum / 3
+        var sum1 = 0
+        var sum3 = 0
+        var sum2 = 0
+        
+        while i + 1 < j {
+            while i < A.count {
+                sum1 += A[i]
+                
+                if sum1 == avg {
+                    break
+                }
+                
+                i += 1
+            }
+            
+            if i < A.count {
+                // 计算第三部分的和
+                while i + 1 < j {
+                    sum3 += A[j]
+                    
+                    if sum3 == avg {
+                        break
+                    }
+                    
+                    j -= 1
+                }
+                
+                if i + 1 < j {
+                    // 计算第二部分和
+                    var k = i + 1
+                    while k < j {
+                        sum2 += A[k]
+                        k += 1
+                    }
+                    
+                    // 继续循环
+                    if sum2 != avg {
+                        i += 1
+                        j -= 1
+                    } else {
+                        return true
+                    }
+                    
+                } else {
+                    return false
+                }
+            } else {
+                return false
+            }
+        }
+        
+        return false
+    }
+}
+
+let s = Solution()
+let A = [3,3,6,5,-2,2,5,1,-9,4,3]
+print(s.canThreePartsEqualSum(A))
