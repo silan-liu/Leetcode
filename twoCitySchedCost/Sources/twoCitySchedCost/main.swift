fb6ec30632925f125774584e3462fb85531c3a2c
silan-liu <413769707@qq.com>
2019-07-08 20:51:38 +0800

    add https://leetcode.com/problems/two-city-scheduling/submissions/

--- twoCitySchedCost/Sources/twoCitySchedCost/main.swift
+++ twoCitySchedCost/Sources/twoCitySchedCost/main.swift
@@ -1,1 +1,23 @@
-print("Hello, world!")
+// https://leetcode.com/problems/two-city-scheduling/submissions/
+class Solution {
+    func twoCitySchedCost(_ costs: [[Int]]) -> Int {
+        let sortedCosts = costs.sorted { (a, b) -> Bool in
+            return (a[1] - a[0]) < (b[1] - b[0])
+        }
+        
+        var i = 0
+        var s = 0
+        
+        while i < sortedCosts.count / 2 {
+            
+            s += sortedCosts[i][1] + sortedCosts[sortedCosts.count - i - 1][0]
+            i += 1
+        }
+        
+        return s
+    }
+}
+
+let s = Solution()
+let costs = [[10,20],[30,10],[30,50],[40,60]]
+print(s.twoCitySchedCost(costs))
