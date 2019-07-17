82af0000638d38c219415dbbdfb3d30769ba2da0
silan-liu <413769707@qq.com>
2019-07-17 11:14:15 +0800

    add BFS solution

--- isCousins/Sources/isCousins/main.swift
+++ isCousins/Sources/isCousins/main.swift
@@ -58,7 +58,68 @@ class Solution {
     }
 }
 
+// 89.39%
+class Solution2 {
+    // BFS
+    func isCousins(_ root: TreeNode?, _ x: Int, _ y: Int) -> Bool {
+        var queue = [TreeNode]()
+        if let root = root {
+            queue.append(root)
+            
+            while !queue.isEmpty {
+                let count = queue.count
+                
+                // 遍历每个元素，此时它们已经是同一层，只需要判断其父节点是否相同
+                var i = 0
+                var xExist = false
+                var yExist = false
+                
+                while i < count {
+                    
+                    let node = queue.first!
+                    queue.removeFirst()
+                    
+                    if node.val == x {
+                        xExist = true
+                    }
+                    
+                    if node.val == y {
+                        yExist = true
+                    }
+                    
+                    // 如果父节点是一样的，false
+                    if node.left?.val == x, node.right?.val == y {
+                        return false
+                    }
+                    
+                    if node.right?.val == x, node.left?.val == y {
+                        return false
+                    }
+                    
+                    if node.left != nil {
+                        queue.append(node.left!)
+                    }
+                    
+                    if node.right != nil {
+                        queue.append(node.right!)
+                    }
+                    
+                    i += 1
+                }
+                
+                if xExist && yExist  {
+                    return true
+                }
+            }
+        }
+        
+        return false
+    }
+}
+
 let s = Solution()
+let s2 = Solution2()
+
 let root = TreeNode(1)
 let left = TreeNode(2)
 let right = TreeNode(3)
@@ -70,3 +131,4 @@ root.left = left
 root.right = right
 
 print(s.isCousins(root, 5, 4))
+print(s2.isCousins(root, 5, 4))
