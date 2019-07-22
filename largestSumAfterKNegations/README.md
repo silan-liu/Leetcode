cfacf8e9166134ae7fe211f660189b6c4d4c7034
silan-liu <413769707@qq.com>
2019-07-22 10:40:09 +0800

    init

---
+++ largestSumAfterKNegations/README.md
@@ -0,0 +1,3 @@
+# largestSumAfterKNegations
+
+A description of this package.
---
+++ largestSumAfterKNegations/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import largestSumAfterKNegationsTests
+
+var tests = [XCTestCaseEntry]()
+tests += largestSumAfterKNegationsTests.allTests()
+XCTMain(tests)
