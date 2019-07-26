d28d1378f80c91239b92393904e9c7d1d14591c6
silan-liu <413769707@qq.com>
2019-07-26 11:20:42 +0800

    init

---
+++ prefixesDivBy5/README.md
@@ -0,0 +1,3 @@
+# prefixesDivBy5
+
+A description of this package.
---
+++ prefixesDivBy5/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import prefixesDivBy5Tests
+
+var tests = [XCTestCaseEntry]()
+tests += prefixesDivBy5Tests.allTests()
+XCTMain(tests)
