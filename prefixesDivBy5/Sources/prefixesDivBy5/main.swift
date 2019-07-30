50662dda296d4f6318cd4dbc84cc74b5ce88bdff
silan-liu <413769707@qq.com>
2019-07-30 20:22:39 +0800

    initial solution

--- prefixesDivBy5/Sources/prefixesDivBy5/main.swift
+++ prefixesDivBy5/Sources/prefixesDivBy5/main.swift
@@ -1,1 +1,21 @@
-print("Hello, world!")
+// https://leetcode.com/problems/binary-prefix-divisible-by-5/
+class Solution {
+    func prefixesDivBy5(_ A: [Int]) -> [Bool] {
+        var result = [Bool]()
+        
+        var last = 0
+        for a in A {
+            let prefix = last << 1 | a
+            print(prefix)
+            result.append(prefix % 5 == 0)
+            
+            last = prefix
+        }
+        
+        return result
+    }
+}
+
+let s = Solution()
+let A = [1,0,1,1,1,1,0,0,0,0,1,0,0,0,0,0,1,0,0,1,1,1,1,1,0,0,0,0,1,1,1,0,0,0,0,0,1,0,0,0,1,0,0,1,1,1,1,1,1,0,1,1,0,1,0,0,0,0,0,0,1,0,1,1,1,0,0,1,0]
+print(s.prefixesDivBy5(A))
