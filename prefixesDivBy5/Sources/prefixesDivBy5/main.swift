ca9feaa99da26edabd33214f00ecbb20926fc573
silan-liu <413769707@qq.com>
2019-07-30 21:20:32 +0800

    add https://leetcode.com/problems/binary-prefix-divisible-by-5/

--- prefixesDivBy5/Sources/prefixesDivBy5/main.swift
+++ prefixesDivBy5/Sources/prefixesDivBy5/main.swift
@@ -1,7 +1,11 @@
 // https://leetcode.com/problems/binary-prefix-divisible-by-5/
 class Solution {
+    // 16.00%
     func prefixesDivBy5(_ A: [Int]) -> [Bool] {
         var result = [Bool]()
+        
+        // 假设 last = 5 * f + r
+        // 下一位 (2 * last + a) % 5 = (2r + a) % 5，等于(上个数/5的余数*2+a) % 5
         
         var last = 0
         for a in A {
@@ -9,7 +13,8 @@ class Solution {
             print(prefix)
             result.append(prefix % 5 == 0)
             
-            last = prefix
+            // 防止溢出
+            last = prefix % 5
         }
         
         return result
