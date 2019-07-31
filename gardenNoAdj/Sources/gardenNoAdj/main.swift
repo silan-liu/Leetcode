eecfac6d3667c6fe35a0b192b265308f47eb1eda
silan-liu <413769707@qq.com>
2019-07-31 19:18:31 +0800

    init

--- gardenNoAdj/Sources/gardenNoAdj/main.swift
+++ gardenNoAdj/Sources/gardenNoAdj/main.swift
@@ -1,1 +1,6 @@
-print("Hello, world!")
+// https://leetcode.com/problems/flower-planting-with-no-adjacent/
+class Solution {
+    func gardenNoAdj(_ N: Int, _ paths: [[Int]]) -> [Int] {
+        
+    }
+}
