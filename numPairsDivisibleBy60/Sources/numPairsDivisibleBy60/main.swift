9c6b8e54bf5ad2c316c7671ec94ab28b2ceea22f
silan-liu <413769707@qq.com>
2019-08-09 20:29:34 +0800

    add solution

--- numPairsDivisibleBy60/Sources/numPairsDivisibleBy60/main.swift
+++ numPairsDivisibleBy60/Sources/numPairsDivisibleBy60/main.swift
@@ -19,6 +19,30 @@ class Solution {
         
         return count
     }
+    
+    // 因为 (x+y) % 60 = 0, x % 60 = 60 - y % 60，而 x % 60 取值范围为 [0,59]，而 60 - y % 60 的取值范围为[0,60]，所以为了两者范围相同，需要 % 60，即 (60 - y % 60) % 60。
+    // 用数组记录 t%60 出现的次数，比如遇到40, t[40] = 1，表示出现了一次，遇到 100, t[40] = 2
+    // 那么当 20 匹配对数时，取t[40]的个数即可。
+    // 87.80%
+    func numPairsDivisibleBy60_2(_ time: [Int]) -> Int {
+        var count = 0
+        var array = [Int]()
+        
+        var i = 0
+        while i < 60 {
+            array.append(0)
+            i += 1
+        }
+        
+        for t in time {
+            count += array[(60 - t % 60) % 60]
+            
+            // 次数加1
+            array[t % 60] += 1
+        }
+        
+        return count
+    }
 }
 
 let s = Solution()
