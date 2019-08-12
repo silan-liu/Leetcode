7657414b16568feb678bd8e29591fa999fcd23f7
silan-liu <413769707@qq.com>
2019-08-12 10:06:31 +0800

    init

---
+++ numEquivDominoPairs/README.md
@@ -0,0 +1,3 @@
+# numEquivDominoPairs
+
+A description of this package.
---
+++ numEquivDominoPairs/Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import numEquivDominoPairsTests
+
+var tests = [XCTestCaseEntry]()
+tests += numEquivDominoPairsTests.allTests()
+XCTMain(tests)
