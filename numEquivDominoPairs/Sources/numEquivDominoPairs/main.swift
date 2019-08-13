6974d2d4694448ec3ae6d5989050a8b61ca9ecdb
silan-liu <413769707@qq.com>
2019-08-13 10:13:19 +0800

    add https://leetcode.com/problems/number-of-equivalent-domino-pairs/

--- numEquivDominoPairs/Sources/numEquivDominoPairs/main.swift
+++ numEquivDominoPairs/Sources/numEquivDominoPairs/main.swift
@@ -1,6 +1,37 @@
 // https://leetcode.com/problems/number-of-equivalent-domino-pairs/
 class Solution {
+    // 80.00%
     func numEquivDominoPairs(_ dominoes: [[Int]]) -> Int {
-        return 0
+        var map = [String: Int]()
+        
+        for domino in dominoes {
+            if domino.count == 2 {
+                let a = domino[0]
+                let b = domino[1]
+                
+                let key = "\(min(a, b))" + "-" + "\(max(a, b))"
+                if let count = map[key] {
+                    map[key] = count + 1
+                } else {
+                    map[key] = 1
+                }
+            }
+        }
+        
+        print(map)
+        
+        var result = 0
+        for (_, value) in map {
+            if value > 1 {
+                // Cn2
+                result += (value * (value - 1)) / 2
+            }
+        }
+        
+        return result
     }
 }
+
+let s = Solution()
+let dominoes = [[1,2],[2,1],[3,4],[4,3]]
+print(s.numEquivDominoPairs(dominoes))
