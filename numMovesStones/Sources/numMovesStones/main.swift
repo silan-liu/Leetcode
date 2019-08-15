f3311b03e0e0b9713f040daa1a0b5130a2569003
silan-liu <413769707@qq.com>
2019-08-15 13:53:02 +0800

    add simple

--- numMovesStones/Sources/numMovesStones/main.swift
+++ numMovesStones/Sources/numMovesStones/main.swift
@@ -39,8 +39,37 @@ class Solution {
         
         return [0, 0]
     }
+    
+    // 68.75%，简化版判断
+    func numMovesStones2(_ a: Int, _ b: Int, _ c: Int) -> [Int] {
+        let sorted = [a, b, c].sorted()
+        
+        // 最小值
+        let x = sorted[0]
+        
+        // 最大值
+        let z = sorted[2]
+        
+        // 中间值
+        let y = sorted[1]
+        
+        // 空位数
+        let d1 = y - x - 1
+        let d2 = z - y - 1
+        
+        // 连续
+        if d1 == 0 && d2 == 0 {
+            return [0, 0]
+        } else if d1 >= 2 && d2 >= 2 {
+            // 位置相隔 >= 2
+            return [2, d1 + d2]
+        } else {
+            return [1, d1 + d2]
+        }
+    }
 }
 
 let s = Solution()
-let a = 2, b = 10, c = 90
+let a = 2, b = 3, c = 5
 print(s.numMovesStones(a, b, c))
+print(s.numMovesStones2(a, b, c))
