f189f39fbfc8f3614fe9e23fb88f34da64b660b2
silan-liu <413769707@qq.com>
2020-01-31 11:32:18 +0800

    add solution

--- container-with-most-water/maxArea.js
+++ container-with-most-water/maxArea.js
@@ -27,5 +27,36 @@ var maxArea = function(height) {
     return max
 };
 
-const height = [1,8,6,2,5,4,8,3,7]
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea2 = function(height) {
+    let i = 0
+    let j = height.length - 1
+    let max = 0
+
+    while (i < j) {
+
+        const minHeight = Math.min(height[i], height[j])
+        const area = minHeight * (j - i)
+
+        if (area > max) {
+            max = area
+        }
+
+        // 如果小于，以 i 为基准计算最大面积，那么从 [i,j] 此时其实是最大的，因为如果 j--，那么面积只会越来越小 。为了计算更大的，需要将 i ++。
+        if (height[i] < height[j]) {
+            i += 1
+        } else {
+            // 反之，类似
+            j -= 1
+        }
+    }
+
+    return max
+};
+
+const height = [1,8,6,2,5,4,8,3,7,8,7,0,6,5,7,2,1]
 console.log(maxArea(height))
+console.log(maxArea2(height))
