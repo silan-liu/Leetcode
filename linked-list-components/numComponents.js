3822f23d6ba64d7b61a0df2df2af6e5ddbb7f3e7
silan-liu <413769707@qq.com>
2020-02-06 20:40:32 +0800

    add v

--- linked-list-components/numComponents.js
+++ linked-list-components/numComponents.js
@@ -1,15 +1,92 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
- */
-/**
+// https://leetcode.com/problems/linked-list-components/submissions/
+// Definition for singly-linked list.
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+
+ /* 
  * @param {ListNode} head
  * @param {number[]} G
  * @return {number}
  */
 var numComponents = function(head, G) {
-    
+    if (head && G && G.length > 0) {
+        let map = new Map()
+
+        // 记录节点前后的值，若无则为 -1
+        let pre = -1
+        while (head) {
+            let next = head.next ? head.next.val : -1
+            map.set(head.val, [pre, next])
+
+            pre = head.val
+            head = head.next
+        }
+
+        let num = 0
+        let i = 0
+        let flagMap = new Map()
+        while (i < G.length) {
+
+            const value = G[i]
+
+            if (!flagMap.get(value)) {
+
+                flagMap.set(value, true)
+
+                // 不是子集
+                if (!map.has(value)) {
+                    return 0
+                }
+
+                num += 1
+
+                const list = map.get(value)
+                let pre = list[0]
+                let next = list[1]
+
+                // 存在 pre
+                while (pre !== -1 && G.includes(pre) && !flagMap.has(pre)) {
+                    flagMap.set(pre, true)
+
+                    // 继续找 pre 的 pre
+                    const list = map.get(pre)
+
+                    pre = list[0]
+                }
+
+                while (next !== -1 && G.includes(next) && !flagMap.has(next)) {
+                    console.log(`has next: ${next}`)
+                    flagMap.set(next, true)
+
+                    const list = map.get(next)
+
+                    next = list[1]
+                }
+            }
+
+            i += 1
+        }
+
+        return num
+    }
+
+    return 0
 };
+
+
+let n0 = new ListNode(0)
+let n1 = new ListNode(1)
+
+let n2 = new ListNode(2)
+let n3 = new ListNode(3)
+let n4 = new ListNode(4)
+
+n0.next = n1
+n1.next = n2
+n2.next = n3
+n3.next = n4
+
+const G = [0, 4, 2, 3]
+console.log(numComponents(n0, G))
