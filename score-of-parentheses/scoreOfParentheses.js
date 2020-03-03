8b08d52b3e65663b7a0f11beffa73d770cd3d75a
silan-liu <413769707@qq.com>
2020-03-03 13:57:51 +0800

    add solution

--- score-of-parentheses/scoreOfParentheses.js
+++ score-of-parentheses/scoreOfParentheses.js
@@ -4,5 +4,44 @@
  * @return {number}
  */
 var scoreOfParentheses = function(S) {
-    
+    return dfs(0, S.length - 1, S)
 };
+
+var dfs = function(l, r, S) {
+    if (l < r && r < S.length) {
+
+        // 从 l 开始遍历，寻找最右匹配 )
+        let start = l
+        let i = l
+        let list = []
+        let sum = 0
+        while (i <= r) {
+            if (S[i] === '(') {
+                list.push(S[i])
+
+            } else if (S[i] === ')') {
+
+                list.pop()
+
+                if (list.length === 0) {
+                    if (i - start === 1) {
+                        sum += 1
+                    } else {
+                        sum += 2 * dfs(start + 1, i - 1, S)
+                    }
+
+                    start = i + 1
+                }
+            }
+
+            i += 1
+        }
+
+        return sum
+    }
+    
+    return 0
+}
+
+const S = '(()((())))((()((()))))(()((())))()(())(())(())(()())'
+console.log(scoreOfParentheses(S))
