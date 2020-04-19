7a9392d3e1f2722d32bd476980e9e4b524c677df
silan-liu <413769707@qq.com>
2020-04-19 10:52:50 +0800

    init

---
+++ exclusive-time-of-functions/exclusiveTime.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/exclusive-time-of-functions/
+/**
+ * @param {number} n
+ * @param {string[]} logs
+ * @return {number[]}
+ */
+var exclusiveTime = function (n, logs) {
+
+};
