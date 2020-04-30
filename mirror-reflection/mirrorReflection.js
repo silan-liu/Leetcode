eba54ade5370f4c05c0369ecaa7b997bc8687cd0
silan-liu <413769707@qq.com>
2020-04-30 20:07:49 +0800

    init

--- mirror-reflection/mirrorReflection.js
+++ mirror-reflection/mirrorReflection.js
@@ -5,5 +5,75 @@
  * @return {number}
  */
 var mirrorReflection = function(p, q) {
-    
+    if (p >= q) {
+        if (p === q) {
+            return 1
+        }
+
+        if (p === 2 * q) {
+            return 2
+        }
+
+        // 已知距离
+        let l = (p - q)
+        let ratio = p / q
+
+        let x = 0
+        let cur = 1
+
+        // 1 - 逆时针，0 - 顺时针
+        let direction = 1
+
+        while (x != p) {
+            if (direction === 1) {
+               cur = (cur + 1) % 4
+            } else {
+                cur = (cur + 3) % 4
+            }
+
+            if (cur % 2 === 1) {
+                x = l / ratio
+            } else {
+                x = l * ratio
+            }
+            
+            if (x > p) {
+                l = x - p
+                
+                if (direction === 1) {
+                    cur = (cur + 1) % 4
+                 } else {
+                    cur = (cur + 3) % 4
+                }
+                
+                // 计算已知距离
+                if (cur % 2 === 1) {
+                    l = l / ratio
+                } else {
+                    l = l * ratio
+                }
+
+                // 如果超出，方向相反
+                direction = (direction + 1) % 2
+
+            } else if (x < p) {
+                l = p - x
+            } else {
+                if (direction === 1) {
+                    cur = cur % 4
+                } else {
+                    cur = (cur + 3) % 4
+                }
+            }
+
+            console.log(cur, x, l, direction)
+        }
+
+        return cur
+    }
+
+    return -1
 };
+
+const p = 12, q = 5
+console.log(mirrorReflection(p, q))
