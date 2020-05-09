4d4c6d8414f5da6a16c30e63da472becf5eea794
silan-liu <413769707@qq.com>
2020-05-09 22:14:31 +0800

    add https://leetcode.com/problems/check-if-it-is-a-straight-line/

---
+++ check-if-it-is-a-straight-line/checkStraightLine.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/check-if-it-is-a-straight-line/
+/**
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+var checkStraightLine = function(coordinates) {
+    
+};
