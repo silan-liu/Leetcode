c7a7c4996cbe2d09172e7d34f163f1648f5dbc4e
silan-liu <413769707@qq.com>
2020-05-26 12:54:39 +0800

    add solution1

--- all-nodes-distance-k-in-binary-tree/distanceK.js
+++ all-nodes-distance-k-in-binary-tree/distanceK.js
@@ -12,6 +12,126 @@ function TreeNode(val) {
  * @return {number[]}
  */
 // https://leetcode.com/problems/all-nodes-distance-k-in-binary-tree/
-var distanceK = function(root, target, K) {
+// 29.96%
+var distanceK = function (root, target, K) {
+    if (root) {
+        let nodeList = []
+        // 先计算以 target 开始的
+        const result = distance(target, 0, K)
+        nodeList = nodeList.concat(result)
+
+        // 再计算路径上的节点
+        const pathList = getPath(root, target)
+        console.log(pathList)
+
+        if (pathList && pathList.length > 0) {
+            let i = 0
+            while (i < pathList.length) {
+                const value = pathList[i]
+                const { node, dir } = value
+
+                // 刚好等于 K
+                if (pathList.length - i === K) {
+                    nodeList.push(node.val)
+                } else {
+                    // 找子树
+                    let startNode = null
+                    if (dir === 0) {
+                        startNode = node.right
+                    } else {
+                        startNode = node.left
+                    }
     
+                    // 剩余长度
+                    const remainDistance = K - (pathList.length - i + 1)
+                    const result = distance(startNode, 0, remainDistance)
+
+                    nodeList = nodeList.concat(result)
+                }
+
+                i += 1
+            }
+        }
+
+        return nodeList
+    }
+
+    return []
 };
+
+var getPath = function (root, target) {
+    if (root) {
+        if (root == target) {
+            console.log('find')
+            return []
+        } else {
+            let result1 = null
+            let result2 = null
+
+            if (root.left) {
+                result1 = getPath(root.left, target)
+            }
+            
+            if (root.right) {
+                result2 = getPath(root.right, target)
+            }
+
+            // console.log(result1, result2)
+
+            if (result1) {
+                const curNode = { node: root, dir: 0 }
+                return [curNode].concat(result1)
+            } else if (result2) {
+                const curNode = { node: root, dir: 1 }
+                return [curNode].concat(result2)
+            }
+
+            return null
+        }
+    }
+
+    return null
+}
+
+// 以 node 开始的，长度为 totalDistance 的节点
+var distance = function (node, curDistance, totalDistance) {
+
+    console.log(node.val, curDistance, totalDistance)
+    if (node) {
+        if (curDistance === totalDistance) {
+            return [node.val]
+        }
+
+        let result = []
+        if (node.left) {
+            const leftResult = distance(node.left, curDistance + 1, totalDistance)
+            result = result.concat(leftResult)
+        }
+
+        if (node.right) {
+            const rightResult = distance(node.right, curDistance + 1, totalDistance)
+            result = result.concat(rightResult)
+        }        
+
+        return result
+    }
+
+    return []
+}
+
+const root = new TreeNode(1)
+const n1 = new TreeNode(2)
+const n2 = new TreeNode(3)
+const n3 = new TreeNode(4)
+const n4 = new TreeNode(5)
+const n5 = new TreeNode(6)
+const n6 = new TreeNode(7)
+
+root.left = n1
+root.right = n2
+n1.left = n3
+n1.right = n5
+n2.left = n6
+n2.right = n4
+
+console.log(distanceK(root, n5, 4))
