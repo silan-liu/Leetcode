2ce1042e843f866b8fc129c9e6ba0fff4497ed3b
silan-liu <413769707@qq.com>
2020-06-02 19:31:52 +0800

    add java solution

--- max-chunks-to-make-sorted/java/maxChunksToSorted/src/App.java
+++ max-chunks-to-make-sorted/java/maxChunksToSorted/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Solution s = new Solution();
+        int[] arr = { 0, 1, 2, 3, 4, 5 };
+
+        int result = s.maxChunksToSorted(arr);
+        System.out.println(result);
     }
 }
--- max-chunks-to-make-sorted/java/maxChunksToSorted/src/Solution.java
+++ max-chunks-to-make-sorted/java/maxChunksToSorted/src/Solution.java
@@ -1,7 +1,28 @@
+// 100.00%
 public class Solution {
     public int maxChunksToSorted(int[] arr) {
-        if (arr.length > 0) {
+        if (arr != null && arr.length > 0) {
 
+            // 保存当前位置最大值
+            int[] maxNum = new int[arr.length];
+            int max = Integer.MIN_VALUE;
+            int result = 0;
+
+            for (int i = 0; i < arr.length; i++) {
+                // 计算最大值
+                max = Math.max(max, arr[i]);
+                maxNum[i] = max;
+            }
+
+            for (int i = 0; i < maxNum.length; i++) {
+
+                // 如果刚好等于排序后的数字，则可以单独成块
+                if (maxNum[i] == i) {
+                    result += 1;
+                }
+            }
+
+            return result;
         }
 
         return 0;
