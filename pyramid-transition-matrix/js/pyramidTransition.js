0a42f60c980da6d07858cfaa151a09462f957043
silan-liu <413769707@qq.com>
2020-06-05 09:49:38 +0800

    init

---
+++ pyramid-transition-matrix/js/pyramidTransition.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string} bottom
+ * @param {string[]} allowed
+ * @return {boolean}
+ */
+// https://leetcode.com/problems/pyramid-transition-matrix/
+var pyramidTransition = function (bottom, allowed) {};
