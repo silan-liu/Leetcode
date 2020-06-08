2a7bbfce5bc00cdd8e04365298c35d1c3e3c1f4b
silan-liu <413769707@qq.com>
2020-06-08 21:31:34 +0800

    update

--- pyramid-transition-matrix/js/pyramidTransition.js
+++ pyramid-transition-matrix/js/pyramidTransition.js
@@ -4,4 +4,27 @@
  * @return {boolean}
  */
 // https://leetcode.com/problems/pyramid-transition-matrix/
-var pyramidTransition = function (bottom, allowed) {};
+var pyramidTransition = function (bottom, allowed) {
+  let allowMap = new Map();
+  allowed.forEach((element) => {
+    // 取前 2 位
+    const prefix = element.slice(0, 2);
+    const value = element[2];
+
+    let list;
+    if (allowMap.has(prefix)) {
+      list = allowMap.get(prefix);
+    } else {
+      list = new Set();
+    }
+
+    list.add(value);
+    allowMap.set(prefix, list);
+  });
+
+  console.log(allowMap);
+};
+
+const bottom = "BCD";
+const allowed = ["BCG", "CDE", "GEA", "FFF"];
+console.log(pyramidTransition(bottom, allowed));
