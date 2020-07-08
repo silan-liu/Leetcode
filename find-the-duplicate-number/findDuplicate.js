23ff314ecac69112a2b21e3373af43bcc9d45472
silan-liu <413769707@qq.com>
2020-07-08 19:40:46 +0800

    init find-the-duplicate-number

---
+++ find-the-duplicate-number/findDuplicate.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// https://leetcode.com/problems/find-the-duplicate-number/
+var findDuplicate = function (nums) {
+  if (nums.length >= 2) {
+  }
+
+  return -1;
+};
+
+const nums = [1, 2, 2, 3];
+console.log(findDuplicate(nums));
