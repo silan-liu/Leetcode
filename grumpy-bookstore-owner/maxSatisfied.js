d46a37e38ec56af7d3563f94b827ad9c43c66c55
silan-liu <413769707@qq.com>
2020-07-23 10:35:05 +0800

    init

---
+++ grumpy-bookstore-owner/maxSatisfied.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} customers
+ * @param {number[]} grumpy
+ * @param {number} X
+ * @return {number}
+ */
+// https://leetcode.com/problems/grumpy-bookstore-owner/
+var maxSatisfied = function(customers, grumpy, X) {
+    
+};
