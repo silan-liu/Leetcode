d9d1554be2d883e2330eef689da8b41897d3208d
silan-liu <413769707@qq.com>
2020-08-04 10:25:13 +0800

    add solution

--- random-pick-index/main.js
+++ random-pick-index/main.js
@@ -2,16 +2,31 @@
  * @param {number[]} nums
  */
 // https://leetcode.com/problems/random-pick-index/
-var Solution = function (nums) {};
+var Solution = function (nums) {
+  this.nums = nums;
+};
 
 /**
  * @param {number} target
  * @return {number}
  */
-Solution.prototype.pick = function (target) {};
+Solution.prototype.pick = function (target) {
+  if (this.nums && this.nums.length > 0) {
+    const targetList = [];
+    this.nums.forEach((num, index) => {
+      if (num === target) {
+        targetList.push(index);
+      }
+    });
 
-/**
- * Your Solution object will be instantiated and called as such:
- * var obj = new Solution(nums)
- * var param_1 = obj.pick(target)
- */
+    if (targetList.length === 1) {
+      return targetList[0];
+    }
+    const randomIndex = Math.floor(Math.random() * 10) % targetList.length;
+    return targetList[randomIndex];
+  }
+};
+
+const nums = [1, 2, 3, 3, 3];
+var obj = new Solution(nums);
+console.log(obj.pick(3));
