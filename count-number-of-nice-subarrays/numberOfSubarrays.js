ade32f1c419e5e66e7abfe30e299bb2875f373d7
silan-liu <413769707@qq.com>
2020-08-26 21:39:55 +0800

    init https://leetcode.com/problems/count-number-of-nice-subarrays

---
+++ count-number-of-nice-subarrays/numberOfSubarrays.js
@@ -0,0 +1,7 @@
+//leetcode.com/problems/count-number-of-nice-subarrays/
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+https: var numberOfSubarrays = function (nums, k) {};
